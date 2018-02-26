a59d91ff787ac87ba0996bec13fe118b39c798e8
zezha-msft <zezha@microsoft.com>
2018-02-26 02:07:28 -0800

    Fixed bug on TransferInfo where the receiver types were wrong

--- ste/runtime-models.go
+++ ste/runtime-models.go
@@ -20,11 +20,11 @@ type TransferInfo struct {
 	NumberOfChunksDone uint32
 }
 
-func (t TransferInfo) getNumberOfChunksDone() uint32 {
+func (t *TransferInfo) getNumberOfChunksDone() uint32 {
 	return atomic.LoadUint32(&t.NumberOfChunksDone)
 }
 
-func (t TransferInfo) ChunksDone() uint32 {
+func (t *TransferInfo) ChunksDone() uint32 {
 	return atomic.AddUint32(&t.NumberOfChunksDone, 1)
 }
 
