c0bf63b772cb49e7a90163345d9493a24151d0b3
Prateek Jain <prjain@microsoft.com>
2018-03-14 11:26:54 -0700

    test blob command changes

--- testSuite/cmd/testblob.go
+++ testSuite/cmd/testblob.go
@@ -8,17 +8,20 @@ import (
 	"os"
 	"context"
 	"crypto/md5"
+	"strings"
 )
 
 type TestBlobCommand struct{
-	Source string
-	Destination string
+	Object          string
+	Subject         string
 	CheckProperties bool
-	MetaData     string
-	ContentType  string
+	MetaData        string
+	ContentType     string
 	ContentEncoding string
 }
 
+// initializes the testblob command, its aliases and description.
+// also adds the possible flags that can be supplied with testBlob command.
 func init(){
 	cmdInput := TestBlobCommand{}
 	testBlobCmd := &cobra.Command{
@@ -30,8 +33,8 @@ func init(){
 			if len(args) != 2 {
 				return fmt.Errorf("invalid arguments for test blob command")
 			}
-			cmdInput.Source = args[0]
-			cmdInput.Destination = args[1]
+			cmdInput.Object = args[0]
+			cmdInput.Subject = args[1]
 			return nil
 		},
 		Run: func(cmd *cobra.Command, args []string) {
@@ -44,10 +47,11 @@ func init(){
 	testBlobCmd.PersistentFlags().StringVar(&cmdInput.ContentType, "contentType", "", "content type expected from the blob in the container")
 }
 
+// Verify Blob gets the
 func verifyBlob(testBlobCmd TestBlobCommand){
-	sourceURL, err := url.Parse(testBlobCmd.Source)
+	sourceURL, err := url.Parse(testBlobCmd.Object)
 	if err != nil{
-		fmt.Println(fmt.Sprintf("Error parsing the blob url source %s", testBlobCmd.Source))
+		fmt.Println(fmt.Sprintf("Error parsing the blob url source %s", testBlobCmd.Object))
 		os.Exit(1)
 	}
 	p := azblob.NewPipeline(azblob.NewAnonymousCredential(), azblob.PipelineOptions{})
@@ -59,14 +63,14 @@ func verifyBlob(testBlobCmd TestBlobCommand){
 	}
 
 	// check whether destination provided is valid or not.
-	fileInfo, err := os.Stat(testBlobCmd.Destination)
+	fileInfo, err := os.Stat(testBlobCmd.Subject)
 	if err != nil{
 		fmt.Println("error opening the destination blob on local disk ")
 		os.Exit(1)
 	}
-	file, err := os.Open(testBlobCmd.Destination)
+	file, err := os.Open(testBlobCmd.Subject)
 	if err != nil{
-		fmt.Println("error opening the file ", testBlobCmd.Destination)
+		fmt.Println("error opening the file ", testBlobCmd.Subject)
 	}
 	fmt.Println("destination file size ", fileInfo.Size())
 	mmap, err := Map(file, false, 0, int(fileInfo.Size()))
@@ -80,5 +84,37 @@ func verifyBlob(testBlobCmd TestBlobCommand){
 	if actualMd5 != expectedMd5 {
 		fmt.Println("the uploaded blob's md5 doesn't matches the actual blob's md5")
 		os.Exit(1)
+	}
+	if testBlobCmd.CheckProperties {
+		// verify the user given metadata supplied while uploading the blob against the metadata actually present in the blob
+		if len(testBlobCmd.MetaData) > 0{
+			// split the meta data string to get the map of key value pair
+			// metadata string is in format key1=value1;key2=value2;key3=value3
+			expectedMetaData := azblob.Metadata{}
+			// split the metadata to get individual keyvalue pair in format key1=value1
+			keyValuePair := strings.Split(testBlobCmd.MetaData, ";")
+			for index := 0; index < len(keyValuePair); index++{
+				// split the individual key value pair to get key and value
+				keyValue := strings.Split(keyValuePair[index], "=")
+				expectedMetaData[keyValue[0]] = keyValue[1]
+			}
+			actualMetaData := blobProperties.NewMetadata()
+			if len(expectedMetaData) != len(actualMetaData){
+				fmt.Println("number of user given key value pair of the actual metadata differs from key value pair of expected metaData")
+				os.Exit(1)
+			}
+			// iterating through each key value pair of actual metaData and comparing the key value pair in expected metadata
+			for key, value := range actualMetaData{
+				if expectedMetaData[key] != value {
+					fmt.Println(fmt.Sprintf("value of user given key %s is %s in actual data while it is %s in expected metadata", key, value, expectedMetaData[key]))
+					os.Exit(1)
+				}
+			}
+		}
+
+		// verify the content-type
+		if len(testBlobCmd.ContentType) != len(blobProperties.ContentType()) {
+
+		}
 	}
 }
