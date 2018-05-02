f57a5e598e2925c34a61d7416670ae0fc691733f
zezha-msft <zezha@microsoft.com>
2018-05-02 01:04:42 -0700

    SHuffle transfers before they are dispatched

--- cmd/copyEnumerator.go
+++ cmd/copyEnumerator.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 
 	"github.com/Azure/azure-storage-azcopy/common"
+	"math/rand"
+	"time"
 )
 
 type copyEnumerator interface {
@@ -21,6 +23,7 @@ func addTransfer(e *common.CopyJobPartOrderRequest, transfer common.CopyTransfer
 	// we do this so that in the case of large uploads, the transfer engine can get started
 	// while the frontend is still gathering more transfers
 	if len(e.Transfers) == NumOfFilesPerUploadJobPart {
+		shuffleTransfers(e.Transfers)
 		resp := common.CopyJobPartOrderResponse{}
 		Rpc(common.ERpcCmd.CopyJobPartOrder(), (*common.CopyJobPartOrderRequest)(e), &resp)
 
@@ -39,8 +42,17 @@ func addTransfer(e *common.CopyJobPartOrderRequest, transfer common.CopyTransfer
 	return nil
 }
 
+// this function shuffles the transfers before they are dispatched
+// this is done to avoid hitting the same partition continuously in an append only pattern
+// TODO this should probably be removed after the high throughput block blob feature is implemented on the service side
+func shuffleTransfers(transfers []common.CopyTransfer) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(transfers), func(i, j int) { transfers[i], transfers[j] = transfers[j], transfers[i] })
+}
+
 // we need to send a last part with isFinalPart set to true, along with whatever transfers that still haven't been sent
 func dispatchFinalPart(e *common.CopyJobPartOrderRequest) error {
+	shuffleTransfers(e.Transfers)
 	e.IsFinalPart = true
 	var resp common.CopyJobPartOrderResponse
 	Rpc(common.ERpcCmd.CopyJobPartOrder(), (*common.CopyJobPartOrderRequest)(e), &resp)
