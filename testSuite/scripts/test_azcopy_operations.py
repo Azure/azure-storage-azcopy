2feffa71a4d579c1c6953cebb921aef7a5c48f67
FAREAST\jiacfan <jiacfan@microsoft.com>
2018-09-18 10:48:10 +0800

    Add case for Oauth remove operation.

--- testSuite/scripts/test_azcopy_operations.py
+++ testSuite/scripts/test_azcopy_operations.py
@@ -25,6 +25,24 @@ class Azcopy_Operation_User_Scenario(unittest.TestCase):
         result = util.Command("list").add_arguments(destination).add_flags("resource-num", "0").execute_azcopy_verify()
         self.assertTrue(result)
 
+    def test_remove_virtual_directory_oauth(self):
+        # create dir dir_10_files and 1 kb files inside the dir.
+        dir_name = "dir_" + str(10) + "_files_rm_oauth"
+        dir_n_files_path = util.create_test_n_files(1024, 10, dir_name)
+
+        # execute azcopy command
+        result = util.Command("copy").add_arguments(dir_n_files_path).add_arguments(util.test_oauth_container_url). \
+            add_flags("recursive", "true").add_flags("log-level", "info").execute_azcopy_copy_command()
+        self.assertTrue(result)
+
+        destination = util.get_object_without_sas(util.test_oauth_container_url, dir_name)
+        result = util.Command("rm").add_arguments(destination).add_flags("recursive", "true").execute_azcopy_copy_command()
+        self.assertTrue(result)
+
+        destination_with_sas = util.get_object_sas(util.test_oauth_container_validate_sas_url, dir_name)
+        result = util.Command("list").add_arguments(destination_with_sas).add_flags("resource-num", "0").execute_azcopy_verify()
+        self.assertTrue(result)
+
 
     def test_remove_files_with_Wildcard(self):
         # create dir dir_remove_files_with_wildcard
