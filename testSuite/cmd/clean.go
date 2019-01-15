458823ef1a05c708112d41cf911f4667bd3632c2
FAREAST\jiacfan <jiacfan@microsoft.com>
2019-01-15 15:13:10 +0800

    Upgrade azfile and azblob to latest version.

--- testSuite/cmd/clean.go
+++ testSuite/cmd/clean.go
@@ -12,7 +12,7 @@ import (
 	"github.com/Azure/azure-pipeline-go/pipeline"
 	"github.com/Azure/azure-storage-azcopy/azbfs"
 	"github.com/Azure/azure-storage-blob-go/azblob"
-	"github.com/Azure/azure-storage-file-go/2017-07-29/azfile"
+	"github.com/Azure/azure-storage-file-go/azfile"
 	"github.com/JeffreyRichter/enum/enum"
 	"github.com/spf13/cobra"
 )
--- testSuite/cmd/testfile.go
+++ testSuite/cmd/testfile.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Azure/azure-storage-file-go/2017-07-29/azfile"
+	"github.com/Azure/azure-storage-file-go/azfile"
 	"github.com/spf13/cobra"
 )
 
