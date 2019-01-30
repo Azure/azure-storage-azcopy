d065f971e19d53db113cf890700df3c1ab3a5119
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2019-01-30 14:36:43 +1300

    Refactor schedulding of upload chunks for testability
    So that the main scheduling routine can be tested in isolation of all the precursor setup stuff

--- ste/xfer-localToRemote.go
+++ ste/xfer-localToRemote.go
@@ -52,13 +52,11 @@ func localToRemote(jptm IJobPartTransferMgr, p pipeline.Pipeline, pacer *pacer, 
 	md5Channel := ul.Md5Channel()
 	defer close(md5Channel) // never leave receiver hanging, waiting for a result, even if we fail here
 
-	// step 2b. Read chunk size and count from the uploader (since it may have applied its own defaults and/or calculations to produce these values
-	chunkSize := ul.ChunkSize()
-	numChunks := ul.NumChunks()
+	// step 2b. Check chunk size and count from the uploader (it may have applied its own defaults and/or calculations to produce these values
 	if jptm.ShouldLog(pipeline.LogInfo) {
-		jptm.LogTransferStart(info.Source, info.Destination, fmt.Sprintf("Specified chunk size %d", chunkSize))
+		jptm.LogTransferStart(info.Source, info.Destination, fmt.Sprintf("Specified chunk size %d", ul.ChunkSize()))
 	}
-	if numChunks == 0 {
+	if ul.NumChunks() == 0 {
 		panic("must always schedule one chunk, even if file is empty") // this keeps our code structure simpler, by using a dummy chunk for empty files
 	}
 
@@ -106,7 +104,7 @@ func localToRemote(jptm IJobPartTransferMgr, p pipeline.Pipeline, pacer *pacer, 
 	// ******
 
 	// step 5: tell jptm what to expect, and how to clean up at the end
-	jptm.SetNumberOfChunks(numChunks)
+	jptm.SetNumberOfChunks(ul.NumChunks())
 	jptm.SetActionAfterLastChunk(func() { epilogueWithCleanupUpload(jptm, ul) })
 
 	// TODO: currently, the epilogue will only run if the number of completed chunks = numChunks.
@@ -118,22 +116,30 @@ func localToRemote(jptm IJobPartTransferMgr, p pipeline.Pipeline, pacer *pacer, 
 	// eventually reach numChunks, since we have no better short-term alternative.
 
 	// Step 5: Go through the file and schedule chunk messages to upload each chunk
-	// As we do this, we force preload of each chunk to memory, and we wait (block)
-	// here if the amount of preloaded data gets excessive. That's OK to do,
-	// because if we already have that much data preloaded (and scheduled for sending in
-	// chunks) then we don't need to schedule any more chunks right now, so the blocking
-	// is harmless (and a good thing, to avoid excessive RAM usage).
-	// To take advantage of the good sequential read performance provided by many file systems,
-	// and to be able to compute an MD5 hash for the file, we work sequentially through the file here.
+	scheduleUploadChunks(jptm, info.Source, srcFile, fileSize, ul, sourceFileFactory, md5Channel)
+}
+
+// Schedule all the upload chunks.
+// As we do this, we force preload of each chunk to memory, and we wait (block)
+// here if the amount of preloaded data gets excessive. That's OK to do,
+// because if we already have that much data preloaded (and scheduled for sending in
+// chunks) then we don't need to schedule any more chunks right now, so the blocking
+// is harmless (and a good thing, to avoid excessive RAM usage).
+// To take advantage of the good sequential read performance provided by many file systems,
+// and to be able to compute an MD5 hash for the file, we work sequentially through the file here.
+func scheduleUploadChunks(jptm IJobPartTransferMgr, srcName string, srcFile common.CloseableReaderAt, fileSize int64, ul uploader, sourceFileFactory common.ChunkReaderSourceFactory, md5Channel chan<- []byte) {
+	chunkSize := ul.ChunkSize()
+	numChunks := ul.NumChunks()
 	context := jptm.Context()
 	slicePool := jptm.SlicePool()
 	cacheLimiter := jptm.CacheLimiter()
+
 	chunkCount := int32(0)
 	md5Hasher := md5.New()
 	safeToUseHash := true
 	for startIndex := int64(0); startIndex < fileSize || isDummyChunkInEmptyFile(startIndex, fileSize); startIndex += int64(chunkSize) {
 
-		id := common.ChunkID{Name: info.Source, OffsetInFile: startIndex}
+		id := common.ChunkID{Name: srcName, OffsetInFile: startIndex}
 		adjustedChunkSize := int64(chunkSize)
 
 		// compute actual size of the chunk
@@ -191,12 +197,10 @@ func localToRemote(jptm IJobPartTransferMgr, p pipeline.Pipeline, pacer *pacer, 
 
 		chunkCount += 1
 	}
-
 	// sanity check to verify the number of chunks scheduled
 	if chunkCount != int32(numChunks) {
-		panic(fmt.Errorf("difference in the number of chunk calculated %v and actual chunks scheduled %v for src %s of size %v", numChunks, chunkCount, info.Source, fileSize))
+		panic(fmt.Errorf("difference in the number of chunk calculated %v and actual chunks scheduled %v for src %s of size %v", numChunks, chunkCount, srcName, fileSize))
 	}
-
 	// provide the hash that we computed
 	if safeToUseHash {
 		md5Channel <- md5Hasher.Sum(nil)
