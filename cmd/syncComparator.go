49c18c337c8a917a823e97a2ba3fb3aff53b8b95
zezha-msft <zezha@microsoft.com>
2019-02-22 17:47:34 -0800

    Refactor sync filters into comparators
    When the destination is traversed second(*), it is impossible to preserve source properties if everything is just done by the destination traverser. (Because it doesn't see source objects).  So in this commit we move the final processing step into a comparator.  Because it can see both objects, it can use the properties of both to correctly schedule the right operation - including preservation of source properties

--- cmd/syncComparator.go
+++ cmd/syncComparator.go
@@ -21,34 +21,40 @@
 package cmd
 
 // with the help of an objectIndexer containing the source objects
-// filter out the destination objects that should be transferred
+// find out the destination objects that should be transferred
 // in other words, this should be used when destination is being enumerated secondly
-type syncDestinationFilter struct {
+type syncDestinationComparator struct {
 	// the rejected objects would be passed to the destinationCleaner
 	destinationCleaner objectProcessor
+
+	// the processor responsible for scheduling copy transfers
+	copyTransferScheduler objectProcessor
 
 	// storing the source objects
 	sourceIndex *objectIndexer
 }
 
-func newSyncDestinationFilter(i *objectIndexer, recyclers objectProcessor) objectFilter {
-	return &syncDestinationFilter{sourceIndex: i, destinationCleaner: recyclers}
+func newSyncDestinationComparator(i *objectIndexer, copyScheduler, cleaner objectProcessor) *syncDestinationComparator {
+	return &syncDestinationComparator{sourceIndex: i, copyTransferScheduler: copyScheduler, destinationCleaner: cleaner}
 }
 
-// it will only pass destination objects that are present in the indexer but stale compared to the entry in the map
-// if the destinationObject is not present at all, it will be passed to the destinationCleaner
+// it will only schedule transfers for destination objects that are present in the indexer but stale compared to the entry in the map
+// if the destinationObject is not at the source, it will be passed to the destinationCleaner
 // ex: we already know what the source contains, now we are looking at objects at the destination
 // if file x from the destination exists at the source, then we'd only transfer it if it is considered stale compared to its counterpart at the source
 // if file x does not exist at the source, then it is considered extra, and will be deleted
-func (f *syncDestinationFilter) doesPass(destinationObject storedObject) bool {
+func (f *syncDestinationComparator) processIfNecessary(destinationObject storedObject) error {
 	sourceObjectInMap, present := f.sourceIndex.indexMap[destinationObject.relativePath]
 
-	// if the destinationObject is present and stale, we let it pass
+	// if the destinationObject is present at source and stale, we transfer the up-to-date version from source
 	if present {
 		defer delete(f.sourceIndex.indexMap, destinationObject.relativePath)
 
 		if sourceObjectInMap.isMoreRecentThan(destinationObject) {
-			return true
+			err := f.copyTransferScheduler(sourceObjectInMap)
+			if err != nil {
+				return err
+			}
 		}
 	} else {
 		// purposefully ignore the error from destinationCleaner
@@ -56,40 +62,45 @@ func (f *syncDestinationFilter) doesPass(destinationObject storedObject) bool {
 		_ = f.destinationCleaner(destinationObject)
 	}
 
-	return false
+	return nil
 }
 
 // with the help of an objectIndexer containing the destination objects
 // filter out the source objects that should be transferred
 // in other words, this should be used when source is being enumerated secondly
-type syncSourceFilter struct {
+type syncSourceComparator struct {
+	// the processor responsible for scheduling copy transfers
+	copyTransferScheduler objectProcessor
 
 	// storing the destination objects
 	destinationIndex *objectIndexer
 }
 
-func newSyncSourceFilter(i *objectIndexer) objectFilter {
-	return &syncSourceFilter{destinationIndex: i}
+func newSyncSourceComparator(i *objectIndexer, copyScheduler objectProcessor) *syncSourceComparator {
+	return &syncSourceComparator{destinationIndex: i, copyTransferScheduler: copyScheduler}
 }
 
-// it will only pass items that are:
+// it will only transfer source items that are:
 //	1. not present in the map
 //  2. present but is more recent than the entry in the map
 // note: we remove the storedObject if it is present so that when we have finished
-// the index will contain all objects which exist at the destination but were NOT passed to this routine
-func (f *syncSourceFilter) doesPass(sourceObject storedObject) bool {
+// the index will contain all objects which exist at the destination but were NOT seen at the source
+func (f *syncSourceComparator) processIfNecessary(sourceObject storedObject) error {
 	destinationObjectInMap, present := f.destinationIndex.indexMap[sourceObject.relativePath]
 
-	// if the sourceObject is more recent, we let it pass
 	if present {
 		defer delete(f.destinationIndex.indexMap, sourceObject.relativePath)
 
+		// if destination is stale, schedule source for transfer
 		if sourceObject.isMoreRecentThan(destinationObjectInMap) {
-			return true
-		}
+			return f.copyTransferScheduler(sourceObject)
 
-		return false
+		} else {
+			// skip if source is more recent
+			return nil
+		}
 	}
 
-	return true
+	// if source does not exist at the destination, then schedule it for transfer
+	return f.copyTransferScheduler(sourceObject)
 }
---
+++ cmd/zt_sync_comparator_test.go
@@ -0,0 +1,139 @@
+// Copyright © 2017 Microsoft <wastore@microsoft.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	chk "gopkg.in/check.v1"
+	"time"
+)
+
+type syncComparatorSuite struct{}
+
+var _ = chk.Suite(&syncComparatorSuite{})
+
+func (s *syncComparatorSuite) TestSyncSourceComparator(c *chk.C) {
+	dummyCopyScheduler := dummyProcessor{}
+	srcMD5 := []byte{'s'}
+	destMD5 := []byte{'d'}
+
+	// set up the indexer as well as the source comparator
+	indexer := newObjectIndexer()
+	sourceComparator := newSyncSourceComparator(indexer, dummyCopyScheduler.process)
+
+	// create a sample destination object
+	sampleDestinationObject := storedObject{name: "test", relativePath: "/usr/test", lastModifiedTime: time.Now(), md5: destMD5}
+
+	// test the comparator in case a given source object is not present at the destination
+	// meaning no entry in the index, so the comparator should pass the given object to schedule a transfer
+	compareErr := sourceComparator.processIfNecessary(storedObject{name: "only_at_source", relativePath: "only_at_source", lastModifiedTime: time.Now(), md5: srcMD5})
+	c.Assert(compareErr, chk.Equals, nil)
+
+	// check the source object was indeed scheduled
+	c.Assert(len(dummyCopyScheduler.record), chk.Equals, 1)
+	c.Assert(dummyCopyScheduler.record[0].md5, chk.DeepEquals, srcMD5)
+
+	// reset the processor so that it's empty
+	dummyCopyScheduler = dummyProcessor{}
+
+	// test the comparator in case a given source object is present at the destination
+	// and it has a later modified time, so the comparator should pass the give object to schedule a transfer
+	err := indexer.store(sampleDestinationObject)
+	c.Assert(err, chk.IsNil)
+	compareErr = sourceComparator.processIfNecessary(storedObject{name: "test", relativePath: "/usr/test", lastModifiedTime: time.Now().Add(time.Hour), md5: srcMD5})
+	c.Assert(compareErr, chk.Equals, nil)
+
+	// check the source object was indeed scheduled
+	c.Assert(len(dummyCopyScheduler.record), chk.Equals, 1)
+	c.Assert(dummyCopyScheduler.record[0].md5, chk.DeepEquals, srcMD5)
+	c.Assert(len(indexer.indexMap), chk.Equals, 0)
+
+	// reset the processor so that it's empty
+	dummyCopyScheduler = dummyProcessor{}
+
+	// test the comparator in case a given source object is present at the destination
+	// but is has an earlier modified time compared to the one at the destination
+	// meaning that the source object is considered stale, so no transfer should be scheduled
+	err = indexer.store(sampleDestinationObject)
+	c.Assert(err, chk.IsNil)
+	compareErr = sourceComparator.processIfNecessary(storedObject{name: "test", relativePath: "/usr/test", lastModifiedTime: time.Now().Add(-time.Hour), md5: srcMD5})
+	c.Assert(compareErr, chk.Equals, nil)
+
+	// check no source object was scheduled
+	c.Assert(len(dummyCopyScheduler.record), chk.Equals, 0)
+	c.Assert(len(indexer.indexMap), chk.Equals, 0)
+}
+
+func (s *syncComparatorSuite) TestSyncDestinationComparator(c *chk.C) {
+	dummyCopyScheduler := dummyProcessor{}
+	dummyCleaner := dummyProcessor{}
+	srcMD5 := []byte{'s'}
+	destMD5 := []byte{'d'}
+
+	// set up the indexer as well as the destination comparator
+	indexer := newObjectIndexer()
+	destinationComparator := newSyncDestinationComparator(indexer, dummyCopyScheduler.process, dummyCleaner.process)
+
+	// create a sample source object
+	sampleSourceObject := storedObject{name: "test", relativePath: "/usr/test", lastModifiedTime: time.Now(), md5: srcMD5}
+
+	// test the comparator in case a given destination object is not present at the source
+	// meaning it is an extra file that needs to be deleted, so the comparator should pass the given object to the destinationCleaner
+	compareErr := destinationComparator.processIfNecessary(storedObject{name: "only_at_dst", relativePath: "only_at_dst", lastModifiedTime: time.Now(), md5: destMD5})
+	c.Assert(compareErr, chk.Equals, nil)
+
+	// verify that destination object is being deleted
+	c.Assert(len(dummyCopyScheduler.record), chk.Equals, 0)
+	c.Assert(len(dummyCleaner.record), chk.Equals, 1)
+	c.Assert(dummyCleaner.record[0].md5, chk.DeepEquals, destMD5)
+
+	// reset dummy processors
+	dummyCopyScheduler = dummyProcessor{}
+	dummyCleaner = dummyProcessor{}
+
+	// test the comparator in case a given destination object is present at the source
+	// and it has a later modified time, since the source data is stale,
+	// no transfer happens
+	err := indexer.store(sampleSourceObject)
+	c.Assert(err, chk.IsNil)
+	compareErr = destinationComparator.processIfNecessary(storedObject{name: "test", relativePath: "/usr/test", lastModifiedTime: time.Now().Add(time.Hour), md5: destMD5})
+	c.Assert(compareErr, chk.Equals, nil)
+
+	// verify that the source object is scheduled for transfer
+	c.Assert(len(dummyCopyScheduler.record), chk.Equals, 0)
+	c.Assert(len(dummyCleaner.record), chk.Equals, 0)
+
+	// reset dummy processors
+	dummyCopyScheduler = dummyProcessor{}
+	dummyCleaner = dummyProcessor{}
+
+	// test the comparator in case a given destination object is present at the source
+	// but is has an earlier modified time compared to the one at the source
+	// meaning that the source object should be transferred since the destination object is stale
+	err = indexer.store(sampleSourceObject)
+	c.Assert(err, chk.IsNil)
+	compareErr = destinationComparator.processIfNecessary(storedObject{name: "test", relativePath: "/usr/test", lastModifiedTime: time.Now().Add(-time.Hour), md5: destMD5})
+	c.Assert(compareErr, chk.Equals, nil)
+
+	// verify that there's no transfer & no deletes
+	c.Assert(len(dummyCopyScheduler.record), chk.Equals, 1)
+	c.Assert(dummyCopyScheduler.record[0].md5, chk.DeepEquals, srcMD5)
+	c.Assert(len(dummyCleaner.record), chk.Equals, 0)
+}
