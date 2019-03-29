cf3f5834d80656d74133b7dab1dc5e07e07a2d33
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2019-03-29 13:23:55 +1300

    Speed up randomDataGenerator

--- common/randomDataGenerator.go
+++ common/randomDataGenerator.go
@@ -22,18 +22,27 @@ package common
 
 import (
 	"errors"
+	"math"
 	"math/rand"
 	"strings"
 	"sync"
 )
 
-// SendRandomDataExt is an optionally specified file suffix, used for generating test data. To use,
-// generate a SPARSE disk file that has zero on-disk size, but a large logical size.
-// Give it a distinctive extension. E.g. myBigFile.azCopySparseFill
-// Then set the command-line parameter to put that extension into this variable.
-// The result will be that AzCopy will upload the full logical size of the sparse file, with all its content as pseudo-random
-// bytes.  So you can have, for example, something that occupies 0 bytes on your disk, but gets uploaded as 10 TB of random data.
-var SendRandomDataExt string
+const (
+	randomSliceLength = 32 * 1024
+)
+
+var (
+	// SendRandomDataExt is an optionally specified file suffix, used for generating test data. To use,
+	// generate a SPARSE disk file that has zero on-disk size, but a large logical size.
+	// Give it a distinctive extension. E.g. myBigFile.azCopySparseFill
+	// Then set the command-line parameter to put that extension into this variable.
+	// The result will be that AzCopy will upload the full logical size of the sparse file, with all its content as pseudo-random
+	// bytes.  So you can have, for example, something that occupies 0 bytes on your disk, but gets uploaded as 10 TB of random data.
+	SendRandomDataExt string
+
+	randomDataBytePool = NewMultiSizeSlicePool(randomSliceLength)
+)
 
 func IsPlaceholderForRandomDataGenerator(filename string) bool {
 	// TODO: Add OS calls to also check here that the on-disk size of the file is zero bytes, so we KNOW for sure
@@ -41,30 +50,88 @@ func IsPlaceholderForRandomDataGenerator(filename string) bool {
 	return SendRandomDataExt != "" && strings.HasSuffix(filename, "."+SendRandomDataExt)
 }
 
-func NewRandomDataGenerator(sizeInBytes int64) CloseableReaderAt {
-	return &randomDataGenerator{sizeInBytes,
-		rand.New(rand.NewSource(rand.Int63())),
-		&sync.Mutex{}}
+func NewRandomDataGenerator(length int64) CloseableReaderAt {
+	r := &randomDataGenerator{
+		length:    length,
+		randGen:   rand.New(rand.NewSource(rand.Int63())),
+		randBytes: randomDataBytePool.RentSlice(randomSliceLength),
+		randMu:    &sync.Mutex{}}
+
+	r.randGen.Read(r.randBytes)
+	return r
 }
 
 type randomDataGenerator struct {
-	size   int64
-	rand   *rand.Rand
-	randMu *sync.Mutex
+	length             int64
+	randGen            *rand.Rand
+	randBytes          []byte
+	randMu             *sync.Mutex
+	readIterationCount int
 }
 
 func (r *randomDataGenerator) Close() error {
+	randomDataBytePool.ReturnSlice(r.randBytes)
+	r.randBytes = nil
 	return nil
 }
 
 func (r *randomDataGenerator) ReadAt(p []byte, off int64) (n int, err error) {
-	if off+int64(len(p)) > r.size {
+	if off+int64(len(p)) > r.length {
 		return 0, errors.New("would read past end")
+	}
+
+	min := func(a int, b int64) int {
+		return int(math.Min(float64(a), float64(b)))
 	}
 
 	// lock, just in case we one day refactor to a design where concurrent reads on one randomDataGenerator may happen
 	r.randMu.Lock()
 	defer r.randMu.Unlock()
 
-	return r.rand.Read(p)
+	n = 0
+	for n < len(p) {
+		remainingInFile := r.length - (off + int64(n))
+		if remainingInFile == 0 {
+			break
+		}
+		remainingThisRead := min(len(p)-n, remainingInFile)
+		remainingThisIteration := min(remainingThisRead, int64(len(r.randBytes)))
+		r.freshenRandomData(remainingThisIteration)
+		copy(p, r.randBytes[:remainingThisIteration])
+		n += remainingThisIteration
+	}
+
+	if n != len(p) {
+		panic("unexpected read length")
+	}
+	return n, nil
+}
+
+// The math.rand type is too slow for us to generate a completely fresh set of random data each time.
+// (I.e. waiting for it to do so throttles our achievable send rate, below the rates we want to test at).
+// So, don't refresh the whole buffer, just refresh a few parts of it - making sure to refresh different parts on each call.
+// Using this approach improves speed from about 2.5 Gbps to about 8 or 9 Gbps (per "file"/randomDataGenerator).
+// The refresh is to
+// (a) prevent the (remote?) possibility of any DDOS protection device or similar becoming suspicious of many absolutely identical payloads, and
+// (b) reduce (but not eliminate?) the risk of something such as a "TCP accelerator", somewhere in the network, compressing our data and thereby
+// giving a misleading perf result.
+func (r *randomDataGenerator) freshenRandomData(count int) {
+	// completely freshen every xth element of the array
+	const arbitraryMediumSizedNumber = 199 // seems sensible if this is relatively prime to the normal size of the payload section of a TCP segment, which is about 1400-and-something
+	r.readIterationCount++
+	for i := r.readIterationCount % arbitraryMediumSizedNumber; i < count; i += arbitraryMediumSizedNumber {
+		r.randGen.Read(r.randBytes[i : i+1])
+	}
+
+	// ALSO flip random bits in every yth one (where y is much smaller than the x we used above)
+	// This is not as random as what we do above, but its faster. And without it, the data is too compressible
+	var skipSize = 2 // with skip-size = 3 its slightly faster, and still uncompressible with zip but it is
+	// compressible (down to 30% of original size) with 7zip's compression
+	bitFlipMask := byte(r.randGen.Int31n(128)) + 128
+	for i := r.readIterationCount % skipSize; i < count; i += skipSize {
+		r.randBytes[i] ^= bitFlipMask
+	}
+
+	// TODO: add unit tests to assert the lack of compressibility (since for now we are just going
+	//   on tests of the .NET code from which randomDataGenerator was ported
 }
