39b36bb83be7d4e673f5f6b1a04d560feccfc65a
zezha-msft <zezha@microsoft.com>
2019-04-20 00:28:38 -0700

    Separated windows-specific tests from zc_traverser_local_test.go

--- cmd/zc_traverser_local_test.go
+++ cmd/zc_traverser_local_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+
 	chk "gopkg.in/check.v1"
 )
 
@@ -10,19 +12,35 @@ var _ = chk.Suite(&localTraverserTestSuite{})
 
 func (s *localTraverserTestSuite) TestCleanLocalPath(c *chk.C) {
 	testCases := map[string]string{
-		"C:\\foo\\bar":   "C:/foo/bar",    // regular windows path with no change
-		"C:\\foo\\bar\\": "C:/foo/bar",    // regular windows path with extra slash
-		".\\foo\\bar":    "foo/bar",       // relative windows path
-		"..\\foo\\bar":   "../foo/bar",    // relative windows path with parent dir
-		"foo\\bar":       "foo/bar",       // shorthand relative windows path
-		"\\\\foo\\bar\\": "//foo/bar",     // network share
-		"C:\\":           "C:/",           // special case, the slash after colon is actually required
-		"D:":             "D:/",           // special case, the slash after colon is actually required
-		"/user/foo/bar":  "/user/foo/bar", // regular unix path with no change
-		"/user/foo/bar/": "/user/foo/bar", // regular unix path with extra slash
-		"./foo/bar":      "foo/bar",       // relative unix path
-		"../foo/bar":     "../foo/bar",    // relative unix path with parent dir
-		"foo/bar":        "foo/bar",       // shorthand relative unix path
+		"/user/foo/bar":    "/user/foo/bar", // regular unix path with no change
+		"/user/foo/bar/":   "/user/foo/bar", // regular unix path with extra slash
+		"/user//foo//bar/": "/user/foo/bar", // regular unix path with double slashes
+		"./foo/bar":        "foo/bar",       // relative unix path
+		"../foo/bar":       "../foo/bar",    // relative unix path with parent dir
+		"foo/bar":          "foo/bar",       // shorthand relative unix path
+	}
+
+	for orig, expected := range testCases {
+		c.Assert(cleanLocalPath(orig), chk.Equals, expected)
+	}
+}
+
+func (s *localTraverserTestSuite) TestCleanLocalPathForWindows(c *chk.C) {
+	// ignore these tests when not running on Windows
+	// as the path cleaning behavior depends on the platform
+	if os.PathSeparator != '\\' {
+		c.SucceedNow()
+	}
+
+	testCases := map[string]string{
+		"C:\\foo\\bar":   "C:/foo/bar", // regular windows path with no change
+		"C:\\foo\\bar\\": "C:/foo/bar", // regular windows path with extra slash
+		".\\foo\\bar":    "foo/bar",    // relative windows path
+		"..\\foo\\bar":   "../foo/bar", // relative windows path with parent dir
+		"foo\\bar":       "foo/bar",    // shorthand relative windows path
+		"\\\\foo\\bar\\": "//foo/bar",  // network share
+		"C:\\":           "C:/",        // special case, the slash after colon is actually required
+		"D:":             "D:/",        // special case, the slash after colon is actually required
 	}
 
 	for orig, expected := range testCases {
