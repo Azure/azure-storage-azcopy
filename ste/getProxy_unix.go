9d93a86e675ae41f2ce3e2d00831fe5019f0b552
Adele Reed <v-adreed@microsoft.com>
2019-05-07 09:54:09 -0700

    Redo getProxy implementation to include ProxyOverride

--- ste/getProxy_unix.go
+++ ste/getProxy_unix.go
@@ -3,10 +3,17 @@
 package ste
 
 import (
+	"golang.org/x/net/http/httpproxy"
 	"net/http"
 	"net/url"
 )
 
-func getProxy() func(*http.Request) (*url.URL, error) {
-	return http.ProxyFromEnvironment
+func getProxy() func(*url.URL) (*url.URL, error) {
+	return httpproxy.FromEnvironment().ProxyFunc()
+}
+
+func proxyFromFunc(f func(*url.URL) (*url.URL, error)) func(*http.Request) (*url.URL, error) {
+	return func(request *http.Request) (*url.URL, error) {
+		return f(request.URL)
+	}
 }
--- ste/getProxy_windows.go
+++ ste/getProxy_windows.go
@@ -1,34 +1,59 @@
 package ste
 
 import (
+	"github.com/Azure/azure-storage-azcopy/common"
 	"golang.org/x/net/http/httpproxy"
 	"golang.org/x/sys/windows/registry"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
-func getProxy() func(*http.Request) (*url.URL, error) {
-	if prox := httpproxy.FromEnvironment(); prox.HTTPProxy != "" || prox.HTTPSProxy != "" || prox.NoProxy != "" {
-		return http.ProxyFromEnvironment
-	}
+//TODO: Make this return httpproxy.config, make function to return properly
+func getProxy() func(*url.URL) (*url.URL, error) {
+	prox := httpproxy.FromEnvironment()
 
 	key, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.QUERY_VALUE)
 	if err != nil {
-		return http.ProxyFromEnvironment
+		return prox.ProxyFunc()
 	}
 	defer key.Close()
 
+	proxyEnableRegKey, _, err := key.GetIntegerValue("ProxyEnable")
+	if err != nil {
+		return prox.ProxyFunc()
+	}
+
+	if prox.HTTPProxy != "" || prox.HTTPSProxy != "" || prox.NoProxy != "" || proxyEnableRegKey == 0 {
+		return prox.ProxyFunc()
+	}
+
 	strURL, _, err := key.GetStringValue("ProxyServer")
 	if err != nil {
-		return http.ProxyFromEnvironment
+		return prox.ProxyFunc()
+	}
+
+	override, _, err := key.GetStringValue("ProxyOverride")
+	if err != nil {
+		return prox.ProxyFunc()
 	}
 
 	proxyURL, err := url.Parse(strURL)
 	if err != nil {
-		return http.ProxyFromEnvironment
+		return prox.ProxyFunc()
 	}
 
-	pURL := http.ProxyURL(proxyURL)
+	cfg := httpproxy.Config{
+		HTTPSProxy: common.IffString(strings.HasPrefix(proxyURL.String(), "https"), proxyURL.String(), ""),
+		HTTPProxy:  common.IffString(strings.HasPrefix(proxyURL.String(), "https"), "", proxyURL.String()),
+		NoProxy:    override,
+	}
 
-	return pURL
+	return cfg.ProxyFunc()
+}
+
+func proxyFromFunc(f func(*url.URL) (*url.URL, error)) func(*http.Request) (*url.URL, error) {
+	return func(request *http.Request) (*url.URL, error) {
+		return f(request.URL)
+	}
 }
