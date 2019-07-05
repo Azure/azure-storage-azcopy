d469bc68ed2e2dd20bfafdcd60fffa44f9c9051a
Adele Reed <v-adreed@microsoft.com>
2019-07-05 14:48:57 -0700

    Set up tests to run with account SAS, not container SAS

--- testSuite/scripts/test_autodetect_blob_type.py
+++ testSuite/scripts/test_autodetect_blob_type.py
@@ -11,7 +11,7 @@ class Autodetect_Blob_Type_Scenario(unittest.TestCase):
         file_path = util.create_test_file(file_name, 4 * 1024 * 1024)
 
         # copy VHD file without specifying page blob. Page blob is inferred for VHD, VHDX, and VMDK
-        destination_sas = util.get_resource_sas(file_name)
+        destination_sas = util.get_object_sas(util.test_container_url, file_name)
         result = util.Command("copy").add_arguments(file_path).add_arguments(destination_sas).add_flags("log-level",
                                                                                                         "info"). \
             add_flags("block-size-mb", 4).execute_azcopy_copy_command()
@@ -24,8 +24,23 @@ class Autodetect_Blob_Type_Scenario(unittest.TestCase):
 
     def test_copy_infer_blob_type_from_block_to_page_blob(self):
         file_name = "testS2SVHD.vhd"
-        src_container_url = util.get_object_sas(util.test_s2s_src_blob_account_url, file_name)
-        dst_container_url = util.get_object_sas(util.test_s2s_dst_blob_account_url, file_name)
+        containerName = util.get_resource_name("s2sbtautodetect")
+
+        # These run on seperate accounts in CI, so even without "dst", it's OK.
+        # Needed this to run on a single account, though.
+        dstbase = util.get_object_sas(util.test_s2s_src_blob_account_url, containerName + "dst")
+        srcbase = util.get_object_sas(util.test_s2s_dst_blob_account_url, containerName)
+
+        result = util.Command("create").add_arguments(srcbase).add_flags("serviceType", "Blob"). \
+            add_flags("resourceType", "Bucket").execute_azcopy_create()
+        self.assertTrue(result)
+
+        result = util.Command("create").add_arguments(dstbase).add_flags("serviceType", "Blob"). \
+            add_flags("resourceType", "Bucket").execute_azcopy_create()
+        self.assertTrue(result)
+
+        src_container_url = util.get_object_sas(srcbase, file_name)
+        dst_container_url = util.get_object_sas(dstbase, file_name)
 
         file_path = util.create_test_file(file_name, 4 * 1024 * 1024)
 
