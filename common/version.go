2add1aebcfa5cb56281b45bb9f6ea03668b7cba5
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2019-09-19 21:19:20 +1200

    Bump version number to a preview-friendly level

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.2.1"
+const AzcopyVersion = "10.2.90"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
