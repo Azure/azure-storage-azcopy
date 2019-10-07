36df8b8a319e3d8d1436d2e8257f82e0ef4aa155
Adele Reed <adreed@microsoft.com>
2019-10-07 09:53:15 -0700

    Supply import for copy_params_debug_fallback.go

--- cmd/copy_params_debug_fallback.go
+++ cmd/copy_params_debug_fallback.go
@@ -2,5 +2,9 @@
 
 package cmd
 
+import (
+	"github.com/spf13/cobra"
+)
+
 // noop func; actual debug params enabled by compiling with -tags debug
 func setupDebugCpParams(CpCmd *cobra.Command, raw *rawCopyCmdArgs) {}
