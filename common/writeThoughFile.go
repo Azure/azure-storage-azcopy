d62efb5f62fadc631a97b386b121069bff1be188
Adele Reed <adreed@microsoft.com>
2019-10-08 15:03:44 -0700

    os.Stat check before attempting mkdirall

--- common/writeThoughFile.go
+++ common/writeThoughFile.go
@@ -33,21 +33,30 @@ import (
 // //myShare
 // //myShare/
 // demonstrated at: https://regexr.com/4mf6l
-var rootDriveRegex = regexp.MustCompile(`(^[A-Z]:\/?$)`)
-var rootShareRegex = regexp.MustCompile(`(^\/\/[^\/]*\/?$)`)
+var RootDriveRegex = regexp.MustCompile(`(^[A-Z]:\/?$)`)
+var RootShareRegex = regexp.MustCompile(`(^\/\/[^\/]*\/?$)`)
 
 func CreateParentDirectoryIfNotExist(destinationPath string) error {
 	// find the parent directory
 	parentDirectory := destinationPath[:strings.LastIndex(destinationPath, DeterminePathSeparator(destinationPath))]
 
 	// If we're pointing at the root of a drive, don't try because it won't work.
-	if shortParentDir := strings.ReplaceAll(ToShortPath(parentDirectory), OS_PATH_SEPARATOR, AZCOPY_PATH_SEPARATOR_STRING); rootDriveRegex.MatchString(shortParentDir) || rootShareRegex.MatchString(shortParentDir) || strings.EqualFold(shortParentDir, "/") {
+	if shortParentDir := strings.ReplaceAll(ToShortPath(parentDirectory), OS_PATH_SEPARATOR, AZCOPY_PATH_SEPARATOR_STRING); RootDriveRegex.MatchString(shortParentDir) || RootShareRegex.MatchString(shortParentDir) || strings.EqualFold(shortParentDir, "/") {
 		return nil
 	} else {
 		fmt.Println(shortParentDir)
 	}
 
-	// try to create the root directory
-	err := os.MkdirAll(parentDirectory, os.ModePerm)
-	return err
+	// try to create the root directory if the source does
+	if _, err := os.Stat(parentDirectory); err != nil {
+		// if the error is present, try to create the directory
+		// stat errors can be present in write-only scenarios, when the directory isn't present, etc.
+		// as a result, we care more about the mkdir error than the stat error, because that's the tell.
+		err := os.MkdirAll(parentDirectory, os.ModePerm)
+		// if MkdirAll succeeds, no error is dropped-- it is nil.
+		// therefore, returning here is perfectly acceptable as it either succeeds (or it doesn't)
+		return err
+	} else { // if err is nil, we return err. if err has an error, we return it.
+		return nil
+	}
 }
