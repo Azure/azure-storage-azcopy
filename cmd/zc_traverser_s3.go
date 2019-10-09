948a6991be2497f3cab6ba48a4784daa409de64f
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2019-10-10 10:54:28 +1300

    Disable (for now) the use of region-less S3 URL

--- cmd/zc_traverser_s3.go
+++ cmd/zc_traverser_s3.go
@@ -22,6 +22,7 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/url"
 	"strings"
@@ -192,6 +193,10 @@ func newS3Traverser(rawURL *url.URL, ctx context.Context, recursive, getProperti
 		t.s3URLParts = s3URLPartsExtension{s3URLParts}
 	}
 
+	if err = checkS3UrlType(s3URLParts); err != nil {
+		return
+	}
+
 	t.s3Client, err = common.CreateS3Client(
 		t.ctx,
 		common.CredentialInfo{
@@ -206,4 +211,17 @@ func newS3Traverser(rawURL *url.URL, ctx context.Context, recursive, getProperti
 		})
 
 	return
+}
+
+// Disallow the non-region aware URL type.
+// Reason is that we had intermittent bucket location lookup issues when using that technique
+// with the apparent cause being the lookup of bucket locations, which we need to change to use minio's BucketExists.
+// For info see: https://github.com/aws/aws-sdk-go/issues/720#issuecomment-243891223
+// Once we change to bucketExists, assuming its reliable, we will be able to re allow this URL type.
+func checkS3UrlType(s3URLParts common.S3URLParts) error {
+	if strings.EqualFold(s3URLParts.Host, "s3.amazonaws.com") {
+		return errors.New("this version of AzCopy does not support transferring from the 's3.amazonaws.com' URL. " +
+			"Please used a region-specific endpoint to transfer from one specific region. E.g. s3.us-east-1.amazonaws.com or a virtual-hosted reference to a single bucket")
+	}
+	return nil
 }
