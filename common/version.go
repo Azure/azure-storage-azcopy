5044c82854e630cb19710081546614d2d57938f5
zezha-msft <zezha@microsoft.com>
2019-10-10 12:44:49 -0700

    Bumped version to 10.3.0

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.2.90"
+const AzcopyVersion = "10.3.0"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
