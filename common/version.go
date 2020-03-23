c17b48b9109d50376a704c0e62eaf171d45fa509
John Rusk [MSFT] <john.rusk@microsoft.com>
2020-03-24 11:31:20 +1300

    Update changelog and bump version number for second internal preview (#923)

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.3.90" // 10.3.90 is preview of 10.4
+const AzcopyVersion = "10.3.91" // 10.3.90 is preview of 10.4
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
