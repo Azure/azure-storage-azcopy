7f1f7f750cc96a1aaa7e415a20ac22b16e000c07
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2020-03-26 10:52:19 +1300

    Bump version number for private build

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.3.4"
+const AzcopyVersion = "10.3.71" // using 70's range for this (series of?) private builds
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
