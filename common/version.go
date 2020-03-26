9d1feb9cbe557b9592182ff22caf41626f838c87
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2020-03-26 19:16:44 +1300

    Update change log and version number

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.3.4"
+const AzcopyVersion = "10.3.61" // using the 6x range for this private drop and any related ones
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
