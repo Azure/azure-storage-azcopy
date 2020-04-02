a3205518dd3ca9dff7eaee4881e234e10de79bb3
adreed-msft <49764384+adreed-msft@users.noreply.github.com>
2020-04-02 14:31:17 -0700

    Fix managed disk corruption risk, if upload two to same URL (#922)
    * Fix managed disk corruption
    
    * Variable naming, comment wording

--- ste/sender-pageBlobFromURL.go
+++ ste/sender-pageBlobFromURL.go
@@ -34,8 +34,8 @@ import (
 type urlToPageBlobCopier struct {
 	pageBlobSenderBase
 
-	srcURL             url.URL
-	pageRangeOptimizer *pageRangeOptimizer // nil if src is not a page blob
+	srcURL                   url.URL
+	sourcePageRangeOptimizer *pageRangeOptimizer // nil if src is not a page blob
 }
 
 func newURLToPageBlobCopier(jptm IJobPartTransferMgr, destination string, p pipeline.Pipeline, pacer pacer, srcInfoProvider IRemoteSourceInfoProvider) (s2sCopier, error) {
@@ -63,16 +63,16 @@ func newURLToPageBlobCopier(jptm IJobPartTransferMgr, destination string, p pipe
 	}
 
 	return &urlToPageBlobCopier{
-		pageBlobSenderBase: *senderBase,
-		srcURL:             *srcURL,
-		pageRangeOptimizer: pageRangeOptimizer}, nil
+		pageBlobSenderBase:       *senderBase,
+		srcURL:                   *srcURL,
+		sourcePageRangeOptimizer: pageRangeOptimizer}, nil
 }
 
 func (c *urlToPageBlobCopier) Prologue(ps common.PrologueState) (destinationModified bool) {
 	destinationModified = c.pageBlobSenderBase.Prologue(ps)
 
-	if c.pageRangeOptimizer != nil {
-		c.pageRangeOptimizer.fetchPages()
+	if c.sourcePageRangeOptimizer != nil {
+		c.sourcePageRangeOptimizer.fetchPages()
 	}
 
 	return
@@ -87,10 +87,18 @@ func (c *urlToPageBlobCopier) GenerateCopyFunc(id common.ChunkID, blockIndex int
 			return
 		}
 
-		// if there's no data at the source, skip this chunk
-		if c.pageRangeOptimizer != nil && !c.pageRangeOptimizer.doesRangeContainData(
-			azblob.PageRange{Start: id.OffsetInFile(), End: id.OffsetInFile() + adjustedChunkSize - 1}) {
-			return
+		// if there's no data at the source (and the destination for managed disks), skip this chunk
+		pageRange := azblob.PageRange{Start: id.OffsetInFile(), End: id.OffsetInFile() + adjustedChunkSize - 1}
+		if c.sourcePageRangeOptimizer != nil && !c.sourcePageRangeOptimizer.doesRangeContainData(pageRange) {
+			var destContainsData bool
+
+			if c.destPageRangeOptimizer != nil {
+				destContainsData = c.destPageRangeOptimizer.doesRangeContainData(pageRange)
+			}
+
+			if !destContainsData {
+				return
+			}
 		}
 
 		// control rate of sending (since page blobs can effectively have per-blob throughput limits)
