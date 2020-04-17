262961046fadfa33a264300661cf1ad85f370757
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2020-04-17 19:01:37 +1200

    Add TODOs re osOpen and osStat

--- common/osOpen_windows.go
+++ common/osOpen_windows.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// TODO: this is not reliable to use for directories, except inside OSStat (since the stat gets the right result)
+//   (But if the raw result of this routine is used, checking its internal dir flag, that flag is wrong for directories)
 func OSOpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
 	// use openwithwritethroughsetting with false writethrough, since it makes a windows syscall containing an ask
 	//     for backup privileges. This allows all of our file opening to go down one route of code.
@@ -23,6 +25,11 @@ func OSOpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
 	return file, nil
 }
 
+// TODO: os.Stat(name string) already uses backup semantics.  Can/should we just use that?
+//  That would work around the issue where here we are using OSOpen, which technically returns a bad result for
+//  directories (but which we seem to get away with in the os.Stat(handle) call - which goes down the "I am a file" code
+//  path, but still returns a valid result for a directory.
+//  See also todo on OsOpen, and edit or remove it
 func OSStat(name string) (os.FileInfo, error) {
 	f, err := OSOpenFile(name, 0, 0)
 
