d3c4daef86af9c48a18a5ac0113fb37d54dc9c1d
Ze Qian Zhang <zezha@microsoft.com>
2020-04-29 17:59:42 -0700

    Fix folder overwrite prompt (#989)

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.4.1"
+const AzcopyVersion = "10.4.2"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
