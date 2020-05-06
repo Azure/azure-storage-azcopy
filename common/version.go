5bd74ae1868310db2db6d202147267fbc0535479
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2020-05-06 16:45:39 +1200

    Bump preview ver num

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.4.81-preview"
+const AzcopyVersion = "10.4.82-preview"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
