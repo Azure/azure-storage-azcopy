e7a6e5006dd7508d99989c1250520b17a614df90
John Rusk <25887678+JohnRusk@users.noreply.github.com>
2020-05-07 09:40:16 +1200

    Revert "Bump ver num for preview"
    This reverts commit b8098598137664339c63adf5a6e724f4b799bd37.

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.4.81-preview"
+const AzcopyVersion = "10.4.3"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
