0b199c1fab5dc63fe1c06a398ac01230e420dadd
Adele Reed <adreed@microsoft.com>
2020-07-02 11:46:11 -0700

    Fix tests

--- cmd/zt_copy_s2smigration_test.go
+++ cmd/zt_copy_s2smigration_test.go
@@ -284,15 +284,7 @@ func (s *cmdIntegrationSuite) TestS2SCopyFromS3ToBlobWithBucketNameNeedBeResolve
 	runCopyAndVerify(c, raw, func(err error) {
 		c.Assert(err, chk.IsNil)
 
-		loggedError := false
-		log := glcm.(*mockedLifecycleManager).infoLog
-		count := len(log)
-		for x := <-log; count > 0; count = len(log) {
-			if strings.Contains(x, "invalid name") {
-				loggedError = true
-			}
-		}
-
+		foundInvalid := false
 		for _, v := range mockedRPC.transfers {
 			if v.EntityType == common.EEntityType.TransferFailure() && strings.Contains(v.FailureReason, "invalid name") {
 				foundInvalid = true
