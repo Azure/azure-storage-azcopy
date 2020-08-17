76532341e94ea8808f0b3606a53c20149a14f574
Mohit Sharma <65536214+mohsha-msft@users.noreply.github.com>
2020-08-17 23:09:50 +0530

    Release V10.6.0 Changelogs (#1141)
    * Release V10.6.0 Changelogs
    
    * Updated message

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.5.101-preview"
+const AzcopyVersion = "10.6.0"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
