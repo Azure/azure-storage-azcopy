7719d5efefddfa0e0c248aef603e3741161178e0
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2020-08-17 15:17:22 +0530

    Jumbo Blob support (#1113)
    * Support Jumbo Blobs
    
    * Jumbo blobs: Improve chunking logic
    
    * Improve chunking logic
    
    * Review Ze's comments
    
    * Add test to verify block size parameters
    
    * Improve error messaging and STE data schema version

--- common/version.go
+++ common/version.go
@@ -1,6 +1,10 @@
 package common
 
+<<<<<<< HEAD
 const AzcopyVersion = "10.6.0"
+=======
+const AzcopyVersion = "10.5.101-preview"
+>>>>>>> ac0c74e... Jumbo Blob support (#1113)
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
