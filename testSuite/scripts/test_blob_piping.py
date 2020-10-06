039c86f895a3973e8baf03577740806884f05f71
Mohit Sharma <65536214+mohsha-msft@users.noreply.github.com>
2020-10-06 08:40:40 +0530

    [AzCopyV10][UX] Output errors on STDERR for piping mode #8031464 (#1185)
    * Changes
    
    * KSH Changes + Refactored error handling
    
    * Code Refactoring
    
    * Fix 1
    
    * Enforce Flags
    
    * Disable Tests for Piping
    
    * Fix Smoke Tests
    
    * Disable piping test - 2
    
    * Fix test cases
    
    * Enable test

--- testSuite/scripts/test_blob_piping.py
+++ testSuite/scripts/test_blob_piping.py
@@ -22,10 +22,11 @@ class BlobPipingTests(unittest.TestCase):
 
         # upload 1KB file using azcopy
         destination_url = util.get_resource_sas(filename)
-        azcopy_cmd = util.Command("copy").add_arguments(destination_url).string()
+        azcopy_cmd = util.Command("copy").add_arguments(destination_url).add_flags("from-to", "PipeBlob").string()
         self.assertTrue(execute_command_with_pipe(azcopy_cmd, source_file_to_pipe=source_file_path))
 
         # downloading the uploaded file
+        azcopy_cmd = util.Command("copy").add_arguments(destination_url).add_flags("from-to", "BlobPipe").string()
         destination_file_path = util.test_directory_path + "/test_1kb_blob_piping_download.txt"
         self.assertTrue(execute_command_with_pipe(azcopy_cmd, destination_file_to_pipe=destination_file_path))
 
@@ -49,10 +50,11 @@ class BlobPipingTests(unittest.TestCase):
         # TODO reviewers please note, this used to use a 4MB file, with a 1 KiB block size, but now we don't support block sizes
         #    smaller than 1 MB. I've compensated slightly by changing it to an 8 MB file
         destination_url = util.get_resource_sas(filename)
-        azcopy_cmd = util.Command("copy").add_arguments(destination_url).add_flags("block-size-mb", '1').string()
+        azcopy_cmd = util.Command("copy").add_arguments(destination_url).add_flags("block-size-mb", '1').add_flags("from-to", "PipeBlob").string()
         self.assertTrue(execute_command_with_pipe(azcopy_cmd, source_file_to_pipe=source_file_path))
 
         # downloading the uploaded file
+        azcopy_cmd = util.Command("copy").add_arguments(destination_url).add_flags("block-size-mb", '1').add_flags("from-to", "BlobPipe").string()
         destination_file_path = util.test_directory_path + "/test_8mb_blob_piping_download.txt"
         self.assertTrue(execute_command_with_pipe(azcopy_cmd, destination_file_to_pipe=destination_file_path))
 
