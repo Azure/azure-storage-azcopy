4e6060edfc83999957a3acb69924b7dfa74e7c29
Mohit Sharma <65536214+mohsha-msft@users.noreply.github.com>
2020-10-21 14:07:24 +0530

    Changelog updates + update version

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.6.1-PRIVATE"
+const AzcopyVersion = "10.6.1"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
