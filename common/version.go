cd672bdce76f8dd22e433c468f40f11cfcb443fb
Mohit Sharma <65536214+mohsha-msft@users.noreply.github.com>
2020-10-28 15:52:42 +0530

    Updated version for private drop

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.6.1-BTS"
+const AzcopyVersion = "10.6.1-BLBTG"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const BenchmarkUserAgent = "Benchmark " + UserAgent
