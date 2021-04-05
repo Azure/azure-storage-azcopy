ba11310508741891441be622f8b3d9d68a990410
Mohit Sharma <65536214+mohsha-msft@users.noreply.github.com>
2021-04-05 10:13:29 +0530

    Minor Comment Change

--- cmd/list.go
+++ cmd/list.go
@@ -296,7 +296,8 @@ func byteSizeToString(size int64) string {
 		"GiB",
 		"TiB",
 		"PiB",
-		"EiB", // Let's face it, a file, account, or container probably won't be more than 1000 exabytes in YEARS. (and int64 literally isn't large enough to handle too many exabytes. 128 bit processors when)
+		"EiB", // Let's face it, a file, account, or container probably won't be more than 1000 exabytes in YEARS.
+		// (and int64 literally isn't large enough to handle too many exbibytes. 128 bit processors when)
 	}
 	unit := 0
 	floatSize := float64(size)
