b2630a522a82c5a8ab91ddfd2d34bd305328d949
zezha-msft <zezha@microsoft.com>
2021-06-01 03:29:50 -0700

    Fix if conditions

--- common/ProxyLookupCache.go
+++ common/ProxyLookupCache.go
@@ -136,7 +136,7 @@ func (c *proxyLookupCache) getProxy(req *http.Request) (*url.URL, error) {
 		value = c.getProxyNoCache(req)
 		c.m.Store(key, value)
 
-		if value.err == nil {
+		if value.err == nil && value.url != nil {
 			// print out a friendly message to let the cx know we've detected a proxy
 			// only do it when we first cached the result so that the message is not printed for every request
 			GetLifecycleMgr().Info(fmt.Sprintf("Proxy detected: %s -> %s", key.String(), value.url.String()))
@@ -176,11 +176,11 @@ func (c *proxyLookupCache) endlessTimedRefresh(key url.URL, representativeFullRe
 		newValue := c.getProxyNoCache(representativeFullRequest)
 		c.m.Store(key, newValue)
 
-		if newValue.err == nil {
+		if newValue.err == nil && newValue.url != nil {
 			// print out a friendly message to let the cx know we've detected a changed proxy if:
 			// 1. the old value had an error and new value doesn't
 			// 2. the proxy url has changed
-			if oldValue.err != nil || (oldValue.err == nil && oldValue.url.String() != newValue.url.String()) {
+			if oldValue.err != nil || oldValue.url == nil || oldValue.url.String() != newValue.url.String() {
 				GetLifecycleMgr().Info(fmt.Sprintf("Proxy detected: %s -> %s", key.String(), newValue.url.String()))
 			}
 		}
