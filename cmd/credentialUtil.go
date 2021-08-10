8ab8f54f981eb8a85085066f31dc0fda4037f43f
simin <siminsavani@microsoft.com>
2021-08-10 12:34:14 -0400

    Adding comment

--- cmd/credentialUtil.go
+++ cmd/credentialUtil.go
@@ -562,7 +562,7 @@ func getCredentialInfoForLocation(ctx context.Context, location common.Location,
 func getCredentialType(ctx context.Context, raw rawFromToInfo, cpkOptions common.CpkOptions) (credType common.CredentialType, err error) {
 
 	switch {
-	case raw.fromTo == common.EFromTo.S3Blob():
+	case raw.fromTo == common.EFromTo.S3Blob(): // S3 Bucket
 		credType, _, err = getCredentialTypeForLocation(ctx, raw.fromTo.From(), raw.source, raw.sourceSAS, true, common.CpkOptions{})
 	case raw.fromTo.To().IsRemote():
 		// we authenticate to the destination. Source is assumed to be SAS, or public, or a local resource
