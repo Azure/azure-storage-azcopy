a58edf55a6b0c1200d4accb99196b32420538fb5
Ze Qian Zhang <zezha@microsoft.com>
2021-09-29 14:45:01 -0700

    Increment version for private drop

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.12.2"
+const AzcopyVersion = "10.13.0-preview"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const GCPImportUserAgent = "GCPImport " + UserAgent
