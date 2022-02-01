6188370188694bbd8c19b74124b1a4bfabe03f26
siminsavani-msft <77068571+siminsavani-msft@users.noreply.github.com>
2022-02-01 12:26:26 -0500

    Modifying basic test

--- e2etest/zt_basic_copy_sync_remove_test.go
+++ e2etest/zt_basic_copy_sync_remove_test.go
@@ -34,7 +34,7 @@ func TestBasic_CopyUploadSingleBlob(t *testing.T) {
 		defaultSize: "1K",
 		shouldTransfer: []interface{}{
 			folder(""),
-			f("filea.txt"),
+			f("file1.txt"),
 		},
 	}, EAccountType.Standard(), EAccountType.Standard(), "")
 }
@@ -46,7 +46,7 @@ func TestBasic_CopyUploadEmptyBlob(t *testing.T) {
 		defaultSize: "0K",
 		shouldTransfer: []interface{}{
 			folder(""),
-			f("filea.txt"),
+			f("file1.txt"),
 		},
 	}, EAccountType.Standard(), EAccountType.Standard(), "")
 }
