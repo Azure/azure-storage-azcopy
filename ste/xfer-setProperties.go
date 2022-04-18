90f3048efce7cb00dea76a69cf7c0753a82d2256
t-iverma <t-iverma@microsoft.com>
2022-04-18 16:03:21 +0530

    Fixing irregularities in xfer-setProperties.go

--- ste/xfer-setProperties.go
+++ ste/xfer-setProperties.go
@@ -3,7 +3,6 @@ package ste
 import (
 	"fmt"
 	"github.com/Azure/azure-pipeline-go/pipeline"
-	"github.com/Azure/azure-storage-azcopy/v10/azbfs"
 	"github.com/Azure/azure-storage-azcopy/v10/common"
 	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/Azure/azure-storage-file-go/azfile"
@@ -57,11 +56,9 @@ func setPropertiesBlob(jptm IJobPartTransferMgr, p pipeline.Pipeline) {
 		jptm.SetStatus(status)
 		jptm.ReportTransferDone()
 	}
-
-	rehydratePriority := jptm.Info().RehydratePriority
+	rehydratePriority := info.RehydratePriority
 	fmt.Println("Rehydrate priority unused- " + rehydratePriority) //this line is a personal reminder and will be removed when https://github.com/Azure/azure-storage-blob-go/pull/319 is merged
 	blockBlobTier, pageBlobTier := jptm.BlobTiers()
-	//srcBlobType := jptm.Info().SrcBlobType
 	PropertiesToTransfer := jptm.PropertiesToTransfer()
 
 	if PropertiesToTransfer.ShouldTransferTier() {
@@ -75,35 +72,27 @@ func setPropertiesBlob(jptm IJobPartTransferMgr, p pipeline.Pipeline) {
 		}
 
 		if err != nil {
-			if strErr, ok := err.(azblob.StorageError); ok {
-				// TODO: Do we need to add more conditions? Won't happen on some snapshots and versions. Check documentation
-
-				// If the status code was 403, it means there was an authentication error, and we exit.
-				// User can resume the job if completely ordered with a new sas.
-				if strErr.Response().StatusCode == http.StatusForbidden {
-					errMsg := fmt.Sprintf("Authentication Failed. The SAS is not correct or expired or does not have the correct permission %s", err.Error())
-					jptm.Log(pipeline.LogError, errMsg)
-					common.GetLifecycleMgr().Error(errMsg)
-				}
-			}
-
-			// in all other cases, make the transfer as failed
-			transferDone(common.ETransferStatus.Failed(), err)
-		} else {
-			transferDone(common.ETransferStatus.Success(), nil)
+			errorHandlerForXferSetProperties(err, jptm, transferDone)
 		}
+		// don't mark it a success just yet, because more properties might need to be changed
 	}
 
 	if PropertiesToTransfer.ShouldTransferMetaData() {
-		panic("Not Supported to be setting metadata yet")
+		var err error = nil
+		err = fmt.Errorf("trying to set metadata - not supported yet")
+		if err != nil {
+			errorHandlerForXferSetProperties(err, jptm, transferDone)
+		}
 	}
+	// marking it a successful flow, as no property has resulted in err != nil
+	transferDone(common.ETransferStatus.Success(), nil)
 }
 
 func setPropertiesBlobFS(jptm IJobPartTransferMgr, p pipeline.Pipeline) {
 	info := jptm.Info()
 	// Get the source blob url of blob to delete
 	u, _ := url.Parse(info.Source)
-	srcBlobURL := azbfs.NewFileURL(*u, p)
+	srcBlobURL := azblob.NewBlobURL(*u, p)
 
 	// Internal function which checks the transfer status and logs the msg respectively.
 	// Sets the transfer status and Report Transfer as Done.
@@ -119,32 +108,38 @@ func setPropertiesBlobFS(jptm IJobPartTransferMgr, p pipeline.Pipeline) {
 		jptm.ReportTransferDone()
 	}
 
-	_, err := srcBlobURL.SetAccessControl(jptm.Context(), azbfs.BlobFSAccessControl{})
-	if err != nil {
-		if strErr, ok := err.(azblob.StorageError); ok {
-			// TODO: Do we need to add more conditions?
+	_, pageBlobTier := jptm.BlobTiers()
+	PropertiesToTransfer := jptm.PropertiesToTransfer()
 
-			// If the status code was 403, it means there was an authentication error, and we exit.
-			// User can resume the job if completely ordered with a new sas.
-			if strErr.Response().StatusCode == http.StatusForbidden {
-				errMsg := fmt.Sprintf("Authentication Failed. The SAS is not correct or expired or does not have the correct permission %s", err.Error())
-				jptm.Log(pipeline.LogError, errMsg)
-				common.GetLifecycleMgr().Error(errMsg)
-			}
+	if PropertiesToTransfer.ShouldTransferTier() {
+		var err error = nil
+		if ValidateTier(jptm, pageBlobTier.ToAccessTierType(), srcBlobURL, jptm.Context()) {
+			_, err = srcBlobURL.SetTier(jptm.Context(), pageBlobTier.ToAccessTierType(), azblob.LeaseAccessConditions{})
 		}
 
-		// in all other cases, make the transfer as failed
-		transferDone(common.ETransferStatus.Failed(), err)
-	} else {
-		transferDone(common.ETransferStatus.Success(), nil)
+		if err != nil {
+			errorHandlerForXferSetProperties(err, jptm, transferDone)
+		}
+		// don't mark it a success just yet, because more properties might need to be changed
 	}
+
+	if PropertiesToTransfer.ShouldTransferMetaData() {
+		var err error = nil
+		err = fmt.Errorf("trying to set metadata - not supported yet")
+		if err != nil {
+			errorHandlerForXferSetProperties(err, jptm, transferDone)
+		}
+	}
+
+	// marking it a successful flow, as no property has resulted in err != nil
+	transferDone(common.ETransferStatus.Success(), nil)
 }
 
 func setPropertiesFile(jptm IJobPartTransferMgr, p pipeline.Pipeline) {
 	info := jptm.Info()
 	u, _ := url.Parse(info.Source)
 	srcFileURL := azfile.NewFileURL(*u, p)
-
+	_ = srcFileURL
 	// Internal function which checks the transfer status and logs the msg respectively.
 	// Sets the transfer status and Report Transfer as Done.
 	// Internal function is created to avoid redundancy of the above steps from several places in the api.
@@ -159,23 +154,35 @@ func setPropertiesFile(jptm IJobPartTransferMgr, p pipeline.Pipeline) {
 		jptm.ReportTransferDone()
 	}
 
-	_, err := srcFileURL.SetMetadata(jptm.Context(), info.SrcMetadata.ToAzFileMetadata())
-	if err != nil {
-		if strErr, ok := err.(azblob.StorageError); ok {
-			// TODO: Do we need to add more conditions?
+	PropertiesToTransfer := jptm.PropertiesToTransfer()
 
-			// If the status code was 403, it means there was an authentication error, and we exit.
-			// User can resume the job if completely ordered with a new sas.
-			if strErr.Response().StatusCode == http.StatusForbidden {
-				errMsg := fmt.Sprintf("Authentication Failed. The SAS is not correct or expired or does not have the correct permission %s", err.Error())
-				jptm.Log(pipeline.LogError, errMsg)
-				common.GetLifecycleMgr().Error(errMsg)
-			}
-		}
-
-		// in all other cases, make the transfer as failed
+	if PropertiesToTransfer.ShouldTransferTier() {
+		err := fmt.Errorf("trying to change tier of file")
 		transferDone(common.ETransferStatus.Failed(), err)
-	} else {
-		transferDone(common.ETransferStatus.Success(), nil)
 	}
+	if PropertiesToTransfer.ShouldTransferMetaData() {
+		var err error = nil
+		err = fmt.Errorf("trying to set metadata - not supported yet")
+		if err != nil {
+			errorHandlerForXferSetProperties(err, jptm, transferDone)
+		}
+	}
+	transferDone(common.ETransferStatus.Success(), nil)
+}
+
+func errorHandlerForXferSetProperties(err error, jptm IJobPartTransferMgr, transferDone func(status common.TransferStatus, err error)) {
+	if strErr, ok := err.(azblob.StorageError); ok {
+		// TODO: Do we need to add more conditions? Won't happen on some snapshots and versions. Check documentation
+
+		// If the status code was 403, it means there was an authentication error, and we exit.
+		// User can resume the job if completely ordered with a new sas.
+		if strErr.Response().StatusCode == http.StatusForbidden {
+			errMsg := fmt.Sprintf("Authentication Failed. The SAS is not correct or expired or does not have the correct permission %s", err.Error())
+			jptm.Log(pipeline.LogError, errMsg)
+			common.GetLifecycleMgr().Error(errMsg)
+		}
+	}
+
+	// in all other cases, make the transfer as failed
+	transferDone(common.ETransferStatus.Failed(), err)
 }
