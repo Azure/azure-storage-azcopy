0014316a4ca60318cedb60f3dc35b6aa23ea1bfc
t-iverma <t-iverma@microsoft.com>
2022-05-14 02:57:18 +0530

    reformatting dry-run code

--- cmd/zc_processor.go
+++ cmd/zc_processor.go
@@ -112,7 +112,8 @@ func (s *copyTransferProcessor) scheduleCopyTransfer(storedObject StoredObject) 
 					return fmt.Sprintf("DRYRUN: remove %v/%v",
 						s.copyJobTemplate.SourceRoot.Value,
 						srcRelativePath)
-				} else if s.copyJobTemplate.FromTo.To() == common.ELocation.None() { //set-properties
+				}
+				if s.copyJobTemplate.FromTo.To() == common.ELocation.None() { //set-properties
 					return fmt.Sprintf("DRYRUN: set-properties %v/%v",
 						s.copyJobTemplate.SourceRoot.Value,
 						srcRelativePath)
