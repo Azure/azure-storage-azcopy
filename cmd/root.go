8346fe90989e181808fe32e937e3da6ca8efa100
Ishaan Verma <99612568+tiverma-msft@users.noreply.github.com>
2022-06-02 16:17:33 +0530

    Avoid creating .azcopy under HOME if plan/log location is specified elsewhere (#1782)
    * avoiding creation of .azcopy if log and plan files have user defined loc
    
    * making azcopyAppPathFolder string irrespective of folder creation
    
    * accessToken to be created where plan files are
    
    * adding TODO
    
    * making same changes in main_unix.go
    
    * getting rid of azcopyAppPathFolder
    
    * making azcopyAppPathFolder before making plan folder to avoid err

--- cmd/root.go
+++ cmd/root.go
@@ -178,8 +178,8 @@ var glcmSwapOnce = &sync.Once{}
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute(azsAppPathFolder, logPathFolder string, jobPlanFolder string, maxFileAndSocketHandles int, jobID common.JobID) {
-	AzcopyAppPathFolder = azsAppPathFolder
+
+func Execute(logPathFolder, jobPlanFolder string, maxFileAndSocketHandles int, jobID common.JobID) {
 	azcopyLogPathFolder = logPathFolder
 	common.AzcopyJobPlanFolder = jobPlanFolder
 	azcopyMaxFileAndSocketHandles = maxFileAndSocketHandles
