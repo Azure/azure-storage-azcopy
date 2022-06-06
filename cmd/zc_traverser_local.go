31d47483afa036087ee8db148fdd0e7d04c92d3c
Strikerzee <arnavtheprasad@gmail.com>
2022-06-06 20:09:30 +0000

    Resolve comments.

--- cmd/zc_traverser_local.go
+++ cmd/zc_traverser_local.go
@@ -35,7 +35,7 @@ import (
 	"github.com/Azure/azure-storage-azcopy/v10/common/parallel"
 )
 
-const MAX_SYMLINKS = 40
+const MAX_SYMLINKS_TO_FOLLOW = 40
 
 type localTraverser struct {
 	fullPath       string
@@ -117,7 +117,7 @@ func UnfurlSymlinks(symlinkPath string) (result string, err error) {
 			 * Either we can store all the symlink seen till now for this path or we count how many iterations to find out cyclic loop.
 			 * Choose the count method and restrict the number of links to 40. Which linux kernel adhere.
 			 */
-			if count >= MAX_SYMLINKS {
+			if count >= MAX_SYMLINKS_TO_FOLLOW {
 				return "", errors.New("failed to unfurl symlink: too many links")
 			}
 
