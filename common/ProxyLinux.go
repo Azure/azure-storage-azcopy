64ea5384d587b32c2fe1a772cff4e78df9ef3e3e
Tamer Sherif <tasherif@microsoft.com>
2023-01-05 11:29:45 -0800

    more testing

--- common/ProxyLinux.go
+++ common/ProxyLinux.go
@@ -1,14 +1,11 @@
-//go:build windows
-// +build windows
-
 package common
 
 import (
-	"github.com/mattn/go-ieproxy"
 	"net/http"
 	"net/url"
 )
 
+// GetProxyFunc is a forwarder for the OS-Exclusive proxyMiddleman_os.go files
 func GetProxyFunc() func(*http.Request) (*url.URL, error) {
-	return ieproxy.GetProxyFunc()
+	return http.ProxyFromEnvironment
 }
---
+++ common/ProxyWindows.go
@@ -0,0 +1,13 @@
+// +build windows
+
+package common
+
+import (
+	"github.com/mattn/go-ieproxy"
+	"net/http"
+	"net/url"
+)
+
+func GetProxyFunc() func(*http.Request) (*url.URL, error) {
+	return ieproxy.GetProxyFunc()
+}
