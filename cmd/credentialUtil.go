944a992e9a355092d04503b0627ff66467bd9caf
Gauri Prasad <gapra@microsoft.com>
2023-01-09 09:28:24 -0800

    remove todo

--- cmd/credentialUtil.go
+++ cmd/credentialUtil.go
@@ -342,7 +342,6 @@ func oAuthTokenExists() (oauthTokenExists bool) {
 
 	if hasCachedToken, err := uotm.HasCachedToken(); hasCachedToken {
 		oauthTokenExists = true
-		// TODO : Add lint ignore for this SA9003
 	} else if err != nil { //nolint:staticcheck
 		// Log the error if fail to get cached token, as these are unhandled errors, and should not influence the logic flow.
 		// Uncomment for debugging.
