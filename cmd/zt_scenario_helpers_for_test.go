4723940e659c5effd9fccaee8aa5204bb10de580
Adele Reed <adreed@microsoft.com>
2023-05-15 14:41:19 -0700

    Set default parameter

--- cmd/zt_scenario_helpers_for_test.go
+++ cmd/zt_scenario_helpers_for_test.go
@@ -870,6 +870,7 @@ func getDefaultSyncRawInput(src, dst string) rawSyncCmdArgs {
 		deleteDestination:   deleteDestination.String(),
 		md5ValidationOption: common.DefaultHashValidationOption.String(),
 		compareHash:         common.ESyncHashType.None().String(),
+		localHashStorageMode: common.EHashStorageMode.Default().String(),
 	}
 }
 
