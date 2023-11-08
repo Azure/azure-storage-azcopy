f1484d53a29e4f5877f17f764a29e5aea0698017
Gauri Prasad <gapra@microsoft.com>
2023-11-07 16:58:21 -0800

    added remove test

--- e2etest/zt_empty_dir_name_test.go
+++ e2etest/zt_empty_dir_name_test.go
@@ -5,8 +5,22 @@ import (
 	"testing"
 )
 
-func TestEmptyDir_CopyUploadSingleBlob(t *testing.T) {
+func TestEmptyDir_CopySyncS2S(t *testing.T) {
 	RunScenarios(t, eOperation.CopyAndSync(), eTestFromTo.Other(common.EFromTo.BlobBlob()), eValidate.AutoPlusContent(), anonymousAuthOnly, anonymousAuthOnly, params{
+		recursive: true,
+	}, nil, testFiles{
+		defaultSize: "1K",
+		shouldTransfer: []interface{}{
+			folder(""),
+			folder("foo"),
+			folder("foo/"),
+			f("foo//file1.txt"),
+		},
+	}, EAccountType.Standard(), EAccountType.Standard(), "")
+}
+
+func TestEmptyDir_RemoveS2S(t *testing.T) {
+	RunScenarios(t, eOperation.Remove(), eTestFromTo.Other(common.EFromTo.BlobTrash()), eValidate.AutoPlusContent(), anonymousAuthOnly, anonymousAuthOnly, params{
 		recursive: true,
 	}, nil, testFiles{
 		defaultSize: "1K",
