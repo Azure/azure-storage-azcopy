90a5f378a08910c99c0c1498a04cd4cf36d771fd
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2024-01-18 17:11:40 +0530

    Add testcase

--- ste/folderCreationTracker.go
+++ ste/folderCreationTracker.go
@@ -53,7 +53,7 @@ func (f *nullFolderTracker) StopTracking(folder string) {
 }
 
 type jpptFolderTracker struct {
-	plan                   *JobPartPlanHeader
+	plan                   IJobPartPlanHeader
 	mu                     *sync.Mutex
 	contents               map[string]uint32
 	unregisteredButCreated map[string]struct{}
@@ -88,6 +88,10 @@ func (f *jpptFolderTracker) CreateFolder(folder string, doCreation func() error)
 	if idx, ok := f.contents[folder]; ok &&
 		f.plan.Transfer(idx).TransferStatus() == (common.ETransferStatus.FolderCreated()) {
 			return nil
+	}
+
+	if _, ok := f.unregisteredButCreated[folder]; ok {
+		return nil
 	}
 	
 	err := doCreation()
