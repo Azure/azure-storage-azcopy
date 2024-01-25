0d984f545f28308f9aa51b1d153e6d7d43ca3432
Pranav Malik <pranavmalik@microsoft.com>
2024-01-25 23:27:44 +0530

    adding code comments

--- ste/sender-appendBlob_test.go
+++ ste/sender-appendBlob_test.go
@@ -110,15 +110,19 @@ func Test500FollowedBy412Logic(t *testing.T) {
 	a.Empty(errString)
 }
 
-func Test404DeleteLogic(t *testing.T) {
+// This function tests the scenario where we return a transfer success even when we receive a 404 response, indicating a resource not found error.
+// In this test, we create a container on an HNS enabled account but do not create any blob. This is done to simulate the 404 scenario when attempting to delete a non-existent blob.
+// The deletion operation won't find the blob to delete, resulting in a 404 error, and thus returning a transfer success.
+func Test404DeleteSuccessLogic(t *testing.T) {
 	a := assert.New(t)
-	//accountName, accountKey := getAccountAndKey()
-	accountName := "nakulkarhns4"
-	accountKey := "E9qRN+7QFREtSLAQB1w+vRrMt2/1bz4XVNqhnVOwdH7Gp8tfFpDGahVmzK6GxbIF34KX4uHOEena/8f24rfnHg=="
+
+	// Setup source and destination
+	accountName, accountKey := getAccountAndKey()
 	rawURL := fmt.Sprintf("https://%s.dfs.core.windows.net/", accountName)
 
 	credential, err := blob.NewSharedKeyCredential(accountName, accountKey)
 	a.Nil(err)
+
 	client, err := blobservice.NewClientWithSharedKeyCredential(rawURL, credential, &blobservice.ClientOptions{
 		ClientOptions: azcore.ClientOptions{
 			Transport: NewAzcopyHTTPClient(0),
@@ -131,6 +135,9 @@ func Test404DeleteLogic(t *testing.T) {
 	a.Nil(err)
 	defer cc.Delete(context.Background(), nil)
 
+	// Generating the name for a blob without actually creating it.
+	sourceName := generateBlobName()
+
 	sasURL, err := cc.NewBlobClient(cName).GetSASURL(
 		blobsas.BlobPermissions{Read: true},
 		time.Now().Add(1*time.Hour),
@@ -141,14 +148,14 @@ func Test404DeleteLogic(t *testing.T) {
 		info: to.Ptr(TransferInfo{
 			Source:       sasURL,
 			SrcContainer: cName,
-			SrcFilePath:  "azure",
+			SrcFilePath:  sourceName,
 		}),
 		fromTo: common.EFromTo.BlobFSTrash(),
 	}
+
 	jptm.SetStatus(common.ETransferStatus.Started())
 	doDeleteHNSResource(jptm)
 
 	a.Nil(err)
-	a.Equal(1, 1)
-
+	a.Equal(jptm.status, common.ETransferStatus.Success())
 }
