35539844f1e7317e0f56584150dc1641fd758b8a
Gauri Prasad <gapra@microsoft.com>
2024-02-07 09:31:15 -0800

    Added tests and updated code to fix issue with page blob downloads

--- ste/sender-pageBlobFromURL.go
+++ ste/sender-pageBlobFromURL.go
@@ -21,11 +21,6 @@
 package ste
 
 import (
-	"context"
-	"strings"
-
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/pageblob"
@@ -161,88 +156,4 @@ func (c *urlToPageBlobCopier) GenerateCopyFunc(id common.ChunkID, blockIndex int
 			return
 		}
 	})
-}
-
-// isolate the logic to fetch page ranges for a page blob, and check whether a given range has data
-// for two purposes:
-//  1. capture the necessary info to do so, so that fetchPages can be invoked anywhere
-//  2. open to extending the logic, which could be re-used for both download and s2s scenarios
-type pageRangeOptimizer struct {
-	srcPageBlobClient *pageblob.Client
-	ctx               context.Context
-	srcPageList       *pageblob.PageList // nil if src is not a page blob, or it was not possible to get a response
-}
-
-func newPageRangeOptimizer(srcPageBlobClient *pageblob.Client, ctx context.Context) *pageRangeOptimizer {
-	return &pageRangeOptimizer{srcPageBlobClient: srcPageBlobClient, ctx: ctx}
-}
-
-// withNoRetryForBlob returns a context that contains a marker to say we don't want any retries to happen
-// Is only implemented for blob pipelines at present
-func withNoRetryForBlob(ctx context.Context) context.Context {
-	return runtime.WithRetryOptions(ctx, policy.RetryOptions{MaxRetries: 1})
-}
-
-func (p *pageRangeOptimizer) fetchPages() {
-	// don't fetch page blob list if optimizations are not desired,
-	// the lack of page list indicates that there's data everywhere
-	if !strings.EqualFold(common.GetLifecycleMgr().GetEnvironmentVariable(
-		common.EEnvironmentVariable.OptimizeSparsePageBlobTransfers()), "true") {
-		return
-	}
-
-	// according to the REST API documentation:
-	// in a highly fragmented page blob with a large number of writes,
-	// a Get Page Ranges request can fail due to an internal server timeout.
-	// thus, if the page blob is not sparse, it's ok for it to fail
-	// TODO follow up with the service folks to confirm the scale at which the timeouts occur
-	// TODO perhaps we need to add more logic here to optimize for more cases
-	limitedContext := withNoRetryForBlob(p.ctx) // we don't want retries here. If it doesn't work the first time, we don't want to chew up (lots) time retrying
-	pager := p.srcPageBlobClient.NewGetPageRangesPager(nil)
-
-	for pager.More() {
-		pageList, err := pager.NextPage(limitedContext)
-		if err == nil {
-			if p.srcPageList == nil {
-				p.srcPageList = &pageList.PageList
-			} else {
-				p.srcPageList.PageRange = append(p.srcPageList.PageRange, pageList.PageRange...)
-				p.srcPageList.ClearRange = append(p.srcPageList.ClearRange, pageList.ClearRange...)
-				p.srcPageList.NextMarker = pageList.NextMarker
-			}
-		}
-	}
-}
-
-// check whether a particular given range is worth transferring, i.e. whether there's data at the source
-func (p *pageRangeOptimizer) doesRangeContainData(givenRange pageblob.PageRange) bool {
-	// if we have no page list stored, then assume there's data everywhere
-	// (this is particularly important when we are using this code not just for performance, but also
-	// for correctness - as we do when using on the destination of a managed disk upload)
-	if p.srcPageList == nil {
-		return true
-	}
-
-	// note that the page list is ordered in increasing order (in terms of position)
-	for _, srcRange := range p.srcPageList.PageRange {
-		if *givenRange.End < *srcRange.Start {
-			// case 1: due to the nature of the list (it's sorted), if we've reached such a srcRange
-			// we've checked all the appropriate srcRange already and haven't found any overlapping srcRange
-			// given range:		|   |
-			// source range:			|   |
-			return false
-		} else if *srcRange.End < *givenRange.Start {
-			// case 2: the givenRange comes after srcRange, continue checking
-			// given range:				|   |
-			// source range:	|   |
-			continue
-		} else {
-			// case 3: srcRange and givenRange overlap somehow
-			// we don't particularly care how it overlaps
-			return true
-		}
-	}
-
-	// went through all srcRanges, but nothing overlapped
-	return false
 }
