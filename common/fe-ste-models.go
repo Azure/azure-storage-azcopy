03b3b1dd34092209f59bc5316ba0a7359c82a6ab
siminsavani-msft <77068571+siminsavani-msft@users.noreply.github.com>
2024-02-08 16:15:52 -0500

    increasing max block size + tests

--- common/fe-ste-models.go
+++ common/fe-ste-models.go
@@ -1071,8 +1071,8 @@ func (i *InvalidMetadataHandleOption) UnmarshalJSON(b []byte) error {
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const (
-	DefaultBlockBlobBlockSize      = 8 * 1024 * 1024
-	MaxBlockBlobBlockSize          = 4000 * 1024 * 1024
+	DefaultBlockBlobBlockSize      = 256 * 1024 * 1024
+	MaxBlockBlobBlockSize          = 5000 * 1024 * 1024
 	MaxAppendBlobBlockSize         = 100 * 1024 * 1024
 	DefaultPageBlobChunkSize       = 4 * 1024 * 1024
 	DefaultAzureFileChunkSize      = 4 * 1024 * 1024
