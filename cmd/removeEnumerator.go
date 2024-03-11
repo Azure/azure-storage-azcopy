3f3e1f010180a3dae1c2d05b248e1c688e86e1da
Gauri Lamunion <51212198+gapra-msft@users.noreply.github.com>
2024-03-11 15:28:20 -0700

    Upgraded to latest az storage sdks (#2599)

--- cmd/removeEnumerator.go
+++ cmd/removeEnumerator.go
@@ -24,11 +24,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake/filesystem"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake/service"
-	"net/http"
 	"path"
 	"strings"
 	"time"
@@ -145,7 +143,7 @@ func removeBfsResources(cca *CookedCopyCmdArgs) (err error) {
 	ctx := context.WithValue(context.Background(), ste.ServiceAPIVersionOverride, ste.DefaultServiceApiVersion)
 	sourceURL, _ := cca.Source.String()
 	options := createClientOptions(common.AzcopyCurrentJobLogger, nil)
-	
+
 	targetServiceClient, err := common.GetServiceClientForLocation(cca.FromTo.From(), sourceURL, cca.credentialInfo.CredentialType, cca.credentialInfo.OAuthTokenInfo.TokenCredential, &options, nil)
 	if err != nil {
 		return err
@@ -240,16 +238,14 @@ func dryrunRemoveSingleDFSResource(ctx context.Context, dsc *service.Client, dat
 	// we do not know if the source is a file or a directory
 	// we assume it is a directory and get its properties
 	directoryClient := dsc.NewFileSystemClient(datalakeURLParts.FileSystemName).NewDirectoryClient(datalakeURLParts.PathName)
-	var respFromCtx *http.Response
-	ctxWithResp := runtime.WithCaptureResponse(ctx, &respFromCtx)
-	_, err := directoryClient.GetProperties(ctxWithResp, nil)
+	props, err := directoryClient.GetProperties(ctx, nil)
 	if err != nil {
 		return fmt.Errorf("cannot verify resource due to error: %s", err)
 	}
 
 	// if the source URL is actually a file
 	// then we should short-circuit and simply remove that file
-	resourceType := respFromCtx.Header.Get("x-ms-resource-type")
+	resourceType := common.IffNotNil(props.ResourceType, "")
 	if strings.EqualFold(resourceType, "file") {
 		glcm.Dryrun(func(_ common.OutputFormat) string {
 			return fmt.Sprintf("DRYRUN: remove file %s", datalakeURLParts.PathName)
