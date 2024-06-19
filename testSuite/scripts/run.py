d6977c6a0f29da294a9dfadf28d7bbbd6a805e82
Gauri Prasad <gapra@microsoft.com>
2024-06-19 12:59:59 -0700

    migrated datalake tests

--- testSuite/scripts/run.py
+++ testSuite/scripts/run.py
@@ -4,9 +4,7 @@ from test_upload_page_blob import *
 from test_file_download import *
 from test_file_upload import *
 from test_blobfs_upload_sharedkey import *
-from test_blobfs_upload_oauth import *
 from test_blobfs_download_sharedkey import *
-from test_blobfs_download_oauth import *
 from test_blob_piping import *
 from test_blob_sync import *
 from test_service_to_service_copy import *
@@ -212,8 +210,6 @@ def main():
                          Block_Upload_User_Scenarios,
                          Blob_Download_User_Scenario,
                          PageBlob_Upload_User_Scenarios,
-                         BlobFs_Upload_OAuth_User_Scenarios,
-                         BlobFs_Download_OAuth_User_Scenarios,
                          BlobFs_Download_SAS_User_Scenarios,
                          BlobFs_Upload_SAS_User_Scenarios,
                          FileShare_Download_User_Scenario,
