7dbfe1ed1edb228252315c6ebf39e00b383c8264
Dhanashree Phulkar <dphulkar@microsoft.com>
2024-07-03 13:20:38 +0530

    incorporated review comments

--- setup/releaseVersionUpdate.py
+++ setup/releaseVersionUpdate.py
@@ -13,35 +13,33 @@ if(len(releaseVersion)==0):
     print('Incorrect Release Version')
     sys.exit(1)
 
+# Split the SAS URL to get the container URL and SAS token
 containerUrl = sasUrl.split('?')[0]
 sasToken = sasUrl.split('?')[1]
 
-# delete latest version file in the container
-deleteUrl = containerUrl + '/' + 'latest_version.txt' + '?' + sasToken
-resp = requests.delete(deleteUrl)
-sys.exit(1) if(resp.status_code<200 or resp.status_code>202) else print('Deleted last release file')
-
-# Create a file and write data to it
+# Create a file and write the release version to it
 file_name = 'latest_version.txt'
-# Open the file in write mode
 with open(file_name, 'w') as file:
     file.write(releaseVersion)
 print(f'Data written to {file_name}')
 
-# Create the BlobServiceClient using the SAS URL
-blob_service_client = BlobServiceClient(account_url=sasUrl.split('?')[0], credential=sasUrl.split('?')[1])
+# Get the full URL to upload the file
+putUrl = containerUrl + '/' + file_name + '?' + sasToken
 
-# Get the container name from the SAS URL
-container_name = sasUrl.split('?')[0].split('/')[-1]
-
-# Get a client to interact with the container
-container_client = blob_service_client.get_container_client(container_name)
-
-# Upload the file to the container
+# Upload the file using a PUT request
 with open(file_name, 'rb') as data:
-    container_client.upload_blob(name=file_name, data=data, overwrite=True)
-
-print(f"File '{file_name}' successfully uploaded to '{container_name}' container.")
+    headers = {
+        'x-ms-blob-type': 'BlockBlob',
+        'Content-Length': str(os.path.getsize(file_name))
+    }
+    resp = requests.put(putUrl, data=data, headers=headers)
+    
+# Check if the request was successful
+if resp.status_code < 200 or resp.status_code > 202:
+    print(f"Failed to upload file: {resp.status_code}, {resp.text}")
+    sys.exit(1)
+else:
+    print('File successfully uploaded')
 
 # Clean up the local file
 os.remove(file_name)
