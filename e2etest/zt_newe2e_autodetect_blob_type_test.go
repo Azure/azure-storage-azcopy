43bb98e609f72b0b33fd27daa4f0d6b54351350a
Gauri Lamunion <51212198+gapra-msft@users.noreply.github.com>
2024-07-16 15:24:31 -0700

    Migrated smoke tests for auto detect blob type (#2729)

---
+++ e2etest/zt_newe2e_autodetect_blob_type_test.go
@@ -0,0 +1,45 @@
+package e2etest
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
+	"github.com/Azure/azure-storage-azcopy/v10/common"
+)
+
+func init() {
+	suiteManager.RegisterSuite(&AutoDetectBlobTypeTestSuite{})
+}
+
+type AutoDetectBlobTypeTestSuite struct{}
+
+func (s *AutoDetectBlobTypeTestSuite) Scenario_AutoInferDetectBlobTypeVHD(svm *ScenarioVariationManager) {
+	fileName := "myVHD.vHd" // awkward capitalization to see if AzCopy catches it.
+	body := NewRandomObjectContentContainer(svm, 4*common.MegaByte)
+
+	srcObj := CreateResource[ContainerResourceManager](svm, GetRootResource(svm, ResolveVariation(svm, []common.Location{common.ELocation.Local(), common.ELocation.File(), common.ELocation.Blob()})), ResourceDefinitionContainer{}).
+		GetObject(svm, fileName, common.EEntityType.File())
+	srcObj.Create(svm, body, ObjectProperties{})
+
+	dstObj := CreateResource[ContainerResourceManager](svm, GetRootResource(svm, common.ELocation.Blob()), ResourceDefinitionContainer{}).
+		GetObject(svm, fileName, common.EEntityType.File())
+
+	// copy vhd file without specifying page blob. Page blob is inferred for VHD, VHDX, and VMDK
+	RunAzCopy(svm, AzCopyCommand{
+		Verb:    AzCopyVerbCopy,
+		Targets: []ResourceManager{srcObj, dstObj},
+		Flags: CopyFlags{
+			CopySyncCommonFlags: CopySyncCommonFlags{
+				Recursive:   pointerTo(true),
+				BlockSizeMB: pointerTo(4.0),
+			},
+		},
+	})
+
+	ValidateResource[ObjectResourceManager](svm, dstObj, ResourceDefinitionObject{
+		Body: body,
+		ObjectProperties: ObjectProperties{
+			BlobProperties: BlobProperties{
+				Type: common.Iff(srcObj.Location() == common.ELocation.Blob(), pointerTo(blob.BlobTypeBlockBlob), pointerTo(blob.BlobTypePageBlob)),
+			},
+		},
+	}, true)
+}
