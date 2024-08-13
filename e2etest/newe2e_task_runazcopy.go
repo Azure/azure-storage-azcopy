670c7c6dc9483e0edbaae785a0012f2ee3b4bf0d
Ubuntu <azureuser@dp-azcopy.oj3u03yqkn1ejhmntyqzjigcgf.tx.internal.cloudapp.net>
2024-08-13 07:30:48 +0000

    Added test case

--- e2etest/newe2e_task_runazcopy.go
+++ e2etest/newe2e_task_runazcopy.go
@@ -313,6 +313,8 @@ func RunAzCopy(a ScenarioAsserter, commandSpec AzCopyCommand) (AzCopyStdout, *Az
 	}
 
 	err = command.Wait()
+	fmt.Println("Error out--------------------------: ", command.Stdout)
+	fmt.Println("Error err--------------------------: ", command.Stderr)
 	fmt.Println("Error--------------------------: ", err)
 	a.Assert("wait for finalize", common.Iff[Assertion](commandSpec.ShouldFail, Not{IsNil{}}, IsNil{}), err)
 	a.Assert("expected exit code",
