31e709ff2f28fa0a435d7732c3535adcf60ed67e
Dhanashree Phulkar <dphulkar@microsoft.com>
2024-10-01 12:04:50 +0530

    Build and push docker image to containers registry

--- docker/publishcontainer.sh
+++ docker/publishcontainer.sh
@@ -3,11 +3,11 @@
 ver=`../azcopy --version | cut -d " " -f 3`
 image="azure-azcopy-$3.$ver"
 
-sudo docker login azcopy.azurecr.io --username $1 --password $2
+sudo docker login azcopycontainers.azurecr.io --username $1 --password $2
 
 # Publish Ubn-22 container image
-sudo docker tag $image:latest azcopy.azurecr.io/$image
-sudo docker push azcopy.azurecr.io/$image
+sudo docker tag $image:latest azcopycontainers.azurecr.io/$image
+sudo docker push azcopycontainers.azurecr.io/$image
 
-sudo docker logout azcopy.azurecr.io
+sudo docker logout azcopycontainers.azurecr.io
 
