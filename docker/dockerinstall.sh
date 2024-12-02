4a11d509806cb077d35c33b968c90aae99784171
Dhanashree Phulkar <dphulkar@microsoft.com>
2024-12-02 12:56:00 +0530

    Fix docker install issue

--- docker/dockerinstall.sh
+++ docker/dockerinstall.sh
@@ -16,7 +16,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 # Create file for installation
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install Docker
+# Install docker
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
