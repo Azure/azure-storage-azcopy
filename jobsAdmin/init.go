b006a07a7bd110b075d45055a7b3f23c85f16462
Shashank Shekhar Nayak <shnayak@microsoft.com>
2025-01-31 18:43:55 -0800

    Adding error message to file transfer details and associated changes.

--- jobsAdmin/init.go
+++ jobsAdmin/init.go
@@ -377,6 +377,7 @@ func ResumeJobOrder(req common.ResumeJobRequest) common.CancelPauseResumeRespons
 				if jppt.TransferStatus() <= common.ETransferStatus.Failed() {
 					jppt.SetTransferStatus(common.ETransferStatus.Restarted(), true)
 					jppt.SetErrorCode(0, true)
+					jppt.SetErrorMessage("", true)
 				}
 			}
 		})
@@ -550,7 +551,8 @@ func resurrectJobSummary(jm ste.IJobMgr) common.ListJobSummaryResponse {
 						Dst:                dst,
 						IsFolderProperties: isFolder,
 						TransferStatus:     common.ETransferStatus.Failed(),
-						ErrorCode:          jppt.ErrorCode()}) // TODO: Optimize
+						ErrorCode:          jppt.ErrorCode(),
+						ErrorMessage:       jppt.ErrorMessage()}) // TODO: Optimize
 			case common.ETransferStatus.SkippedEntityAlreadyExists(),
 				common.ETransferStatus.SkippedBlobHasSnapshots():
 				js.TransfersSkipped++
@@ -671,7 +673,13 @@ func ListJobTransfers(r common.ListJobTransfersRequest) common.ListJobTransfersR
 			// getting source and destination of a transfer at index index for given jobId and part number.
 			src, dst, isFolder := jpp.TransferSrcDstStrings(t)
 			ljt.Details = append(ljt.Details,
-				common.TransferDetail{Src: src, Dst: dst, IsFolderProperties: isFolder, TransferStatus: transferEntry.TransferStatus(), ErrorCode: transferEntry.ErrorCode()})
+				common.TransferDetail{
+					Src:                src,
+					Dst:                dst,
+					IsFolderProperties: isFolder,
+					TransferStatus:     transferEntry.TransferStatus(),
+					ErrorCode:          transferEntry.ErrorCode(),
+					ErrorMessage:       transferEntry.ErrorMessage()})
 		}
 	}
 	return ljt
