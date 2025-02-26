8cce2efa790ce5ae876fe1ee9617bca7df7d0657
Adele Reed <virepri2k@gmail.com>
2025-02-26 00:01:43 -0800

    Refactor device login test

--- e2etest/zt_newe2e_token_persist_test.go
+++ e2etest/zt_newe2e_token_persist_test.go
@@ -1,6 +1,7 @@
 package e2etest
 
 import (
+	"flag"
 	"fmt"
 	"github.com/Azure/azure-storage-azcopy/v10/common"
 )
@@ -68,5 +69,64 @@ func (*TokenPersistenceSuite) Scenario_InheritCred_Persist(a *ScenarioVariationM
 		AzCopyCommand{
 			Verb:        AzCopyVerbLogout,
 			Environment: azcopyEnv,
+		})
+}
+
+var runDeviceCodeTest = flag.Bool("device-code", false, "Whether or not to run device code tests. These must be run manually due to interactive nature.")
+
+func (*TokenPersistenceSuite) Scenario_DeviceCode_Persist(a *ScenarioVariationManager) {
+	if runDeviceCodeTest == nil || !*runDeviceCodeTest {
+		a.Skip("device code disabled")
+	}
+
+	env := &AzCopyEnvironment{
+		LoginCacheName: pointerTo("AzCopyDeviceCodeTest"),
+		ManualLogin:    true,
+	}
+
+	cfgTenantID := GlobalConfig.GetTenantID()
+	_, _ = RunAzCopy(a,
+		AzCopyCommand{
+			Verb: AzCopyVerbLogin,
+			Flags: LoginFlags{
+				LoginType: pointerTo(common.EAutoLoginType.Device()),
+				TenantID:  common.Iff(cfgTenantID != "", &cfgTenantID, nil),
+			},
+			Environment: env,
+		})
+
+	loginStatus, _ := RunAzCopy(a,
+		AzCopyCommand{
+			Verb: AzCopyVerbLoginStatus,
+			Flags: LoginStatusFlags{
+				Method:   pointerTo(true),
+				Endpoint: pointerTo(true),
+				Tenant:   pointerTo(true),
+			},
+			Environment: env,
+		})
+
+	parsedStdout, ok := loginStatus.(*AzCopyParsedLoginStatusStdout)
+	a.AssertNow("must be AzCopyParsedLoginStatusStdout", Equal{}, ok, true)
+
+	if !a.Dryrun() {
+		status := parsedStdout.status
+		a.Assert("Login check failed", Equal{}, true, status.Valid)
+
+		a.Assert("Tenant not returned", Not{IsNil{}}, status.TenantID) // Let's just do a little extra testing while we're at it, kill two birds with one stone.
+		if status.TenantID != nil && cfgTenantID != "" {
+			a.Assert("Tenant does not match", Equal{}, cfgTenantID, *status.TenantID)
+		}
+		a.Assert("Endpoint not returned", Not{IsNil{}}, status.AADEndpoint)
+		a.Assert("Auth mechanism not returned", Not{IsNil{}}, status.AuthMethod)
+		if status.AuthMethod != nil {
+			a.Assert("Incorrect auth mechanism", Equal{}, *status.AuthMethod, common.EAutoLoginType.Device().String())
+		}
+	}
+
+	_, _ = RunAzCopy(a,
+		AzCopyCommand{
+			Verb:        AzCopyVerbLogout,
+			Environment: env,
 		})
 }
