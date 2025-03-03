35a9d57cdc74b1cd5d76ad52acd8fb02b509c290
Adele Reed <virepri2k@gmail.com>
2025-03-02 21:29:31 -0800

    nil catch

--- ste/xferRetryHelper_windows.go
+++ ste/xferRetryHelper_windows.go
@@ -11,6 +11,10 @@ import (
 
 func init() {
 	platformRetryPolicy = func(response *http.Response, err error) bool {
+		if err == nil {
+			return false // we have no tests to run against this
+		}
+
 		// It's entirely possible something in between closed our connection.
 		if errors.Is(err, windows.WSAECONNRESET) || // Catch it in the idiomatic way
 			strings.Contains(strings.ToLower(err.Error()), "an existing connection was forcibly closed by the remote host.") { // But just in case something funny happened along the line, let's listen for the string we expect.
