975ab32cd209d850776810f9390faabc2c1e531d
Shashank Shekhar Nayak <shnayak@microsoft.com>
2025-07-07 02:26:44 -0700

    Implementing GenerateCopyMetadata for AzureFiles and BlobFS for enabling metadata only transfer

--- ste/sender-azureFileFromLocal.go
+++ ste/sender-azureFileFromLocal.go
@@ -22,6 +22,7 @@ package ste
 
 import (
 	"fmt"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azfile/file"
 	"github.com/Azure/azure-storage-azcopy/v10/common"
 )
@@ -89,11 +90,89 @@ func (u *azureFileUploader) Epilogue() {
 
 			u.headersToApply.ContentMD5 = md5Hash
 			_, err := u.getFileClient().SetHTTPHeaders(u.ctx, &file.SetHTTPHeadersOptions{
-				HTTPHeaders: &u.headersToApply,
-				Permissions: &u.permissionsToApply,
+				HTTPHeaders:   &u.headersToApply,
+				Permissions:   &u.permissionsToApply,
 				SMBProperties: &u.smbPropertiesToApply,
 			})
 			return err
 		})
 	}
+}
+
+func (s *azureFileUploader) GenerateCopyMetadata(id common.ChunkID) chunkFunc {
+	return createChunkFunc(true, s.jptm, id, func() {
+		info := s.jptm.Info()
+		var err error
+
+		if info.IsNFSCopy {
+			_, err = s.addNFSPermissionsToHeaders(info, s.getFileClient().URL())
+			if err != nil {
+				s.jptm.FailActiveSend("Setting file permissions", err)
+				return
+			}
+
+			_, err = s.addNFSPropertiesToHeaders(info)
+			if err != nil {
+				s.jptm.FailActiveSend("Setting file properties", err)
+				return
+			}
+
+		} else {
+			_, err = s.addPermissionsToHeaders(info, s.getFileClient().URL())
+			if err != nil {
+				s.jptm.FailActiveSend("Setting file permissions", err)
+				return
+			}
+
+			_, err = s.addSMBPropertiesToHeaders(info)
+			if err != nil {
+				s.jptm.FailActiveSend("Setting file properties", err)
+				return
+			}
+		}
+
+		err = common.DoWithOverrideReadOnlyOnAzureFiles(s.ctx,
+			func() (interface{}, error) {
+				var resp interface{}
+				var err error
+				_, err = s.getFileClient().SetMetadata(s.ctx, &file.SetMetadataOptions{Metadata: s.metadataToApply})
+				if err != nil {
+					return nil, err
+				}
+				if s.jptm.Info().IsNFSCopy {
+					resp, err = s.getFileClient().SetHTTPHeaders(s.ctx, &file.SetHTTPHeadersOptions{
+						HTTPHeaders: &s.headersToApply,
+						NFSProperties: &file.NFSProperties{
+							CreationTime:  s.nfsPropertiesToApply.CreationTime,
+							LastWriteTime: s.nfsPropertiesToApply.LastWriteTime,
+							FileMode:      s.nfsPropertiesToApply.FileMode,
+							Owner:         s.nfsPropertiesToApply.Owner,
+							Group:         s.nfsPropertiesToApply.Group,
+						},
+					})
+					if err != nil {
+						s.jptm.FailActiveSend("Applying final attribute settings", err)
+						return nil, err
+					}
+				} else {
+					resp, err = s.getFileClient().SetHTTPHeaders(s.ctx, &file.SetHTTPHeadersOptions{
+						HTTPHeaders:   &s.headersToApply,
+						Permissions:   &s.permissionsToApply,
+						SMBProperties: &s.smbPropertiesToApply,
+					})
+					if err != nil {
+						s.jptm.FailActiveSend("Applying final attribute settings", err)
+						return nil, err
+					}
+				}
+				return resp, nil
+			},
+			s.fileOrDirClient,
+			s.jptm.GetForceIfReadOnly())
+
+		if err != nil {
+			s.jptm.FailActiveUpload("Applying final attribute settings", err)
+			return
+		}
+	})
 }
