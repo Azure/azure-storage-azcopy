3f3a5fea13941449012f19e691139b50b4d82298
Shashank Shekhar Nayak <shnayak@microsoft.com>
2025-07-14 15:25:11 -0700

    New E2E test framework - bug fixes - Adding missing defer statement - Append env in RegisterEnvironment similar to CreateEnvironment. Custom environments are failing due to this.

--- e2etest/newe2e_azcopy_environment_manager.go
+++ e2etest/newe2e_azcopy_environment_manager.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/google/uuid"
 	"io"
 	"io/fs"
 	"os"
@@ -12,6 +11,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type AzCopyEnvironmentManagerKey struct{}
@@ -127,13 +128,14 @@ func (envCtx *AzCopyEnvironmentContext) RegisterEnvironment(env *AzCopyEnvironme
 
 	env.EnvironmentId = pointerTo(uint(len(envCtx.Environments)))
 	env.ParentContext = envCtx
+	envCtx.Environments = append(envCtx.Environments, env)
 
 	return rc
 }
 
 func (envCtx *AzCopyEnvironmentContext) RegisterLogUpload(upload LogUpload) {
 	envCtx.mu.Lock()
-	envCtx.mu.Unlock()
+	defer envCtx.mu.Unlock()
 
 	envCtx.LogUploads = append(envCtx.LogUploads, upload)
 }
@@ -162,6 +164,11 @@ func (envCtx *AzCopyEnvironmentContext) DoCleanup(a Asserter) {
 
 	if a.Failed() && GlobalConfig.AzCopyExecutableConfig.LogDropPath != "" {
 		for _, logUpload := range envCtx.LogUploads {
+			if int(logUpload.EnvironmentID) >= len(envCtx.Environments) {
+				a.Log("Warning: LogUpload references non-existent environment ID %d (have %d environments)",
+					logUpload.EnvironmentID, len(envCtx.Environments))
+				continue
+			}
 			env := envCtx.Environments[logUpload.EnvironmentID]
 			envUploadDir := envCtx.GetEnvUploadPath(env)
 
@@ -282,5 +289,5 @@ func (env *AzCopyEnvironment) DoCleanup(a Asserter) {
 	a.NoError("failed to copy plans", err)
 
 	uploadRelPath := strings.TrimPrefix(logDropPath, GlobalConfig.AzCopyExecutableConfig.LogDropPath)
-	a.Log("Uploaded logs for session to %s", uploadRelPath)
+	a.Log("Scenario %s: Uploaded logs for session to %s", env.ParentContext.ScenarioID.String(), uploadRelPath)
 }
