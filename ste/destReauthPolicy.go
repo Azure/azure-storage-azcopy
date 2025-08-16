f802ea4957b0fbfe5fbe4fbcedd4482dbfda3ea2
Wendi Onwuakpa <wonwuakpa@microsoft.com>
2025-08-15 20:37:22 -0400

    nil check

--- ste/destReauthPolicy.go
+++ ste/destReauthPolicy.go
@@ -50,74 +50,76 @@ retry:
 	clone := req.Clone(ctx)
 	resp, err := clone.Next() // Initially attempt the request.
 
-	if err != nil || resp.StatusCode != http.StatusOK { // But, if we get back an error...
-		var authReq *azidentity.AuthenticationRequiredError
-		var respErr = &azcore.ResponseError{}
+	if resp != nil { // in case of timeouts and network failures
+		if err != nil || resp.StatusCode != http.StatusOK { // But, if we get back an error...
+			var authReq *azidentity.AuthenticationRequiredError
+			var respErr = &azcore.ResponseError{}
 
-		reauth := false
+			reauth := false
 
-		switch { // Is it an error we can resolve by re-authing?
-		case errors.As(err, &authReq):
-			reauth = true
-			debugCtx = context.WithValue(debugCtx, destReauthDebugCause, destReauthDebugCauseAuthenticationRequired)
-		case resp.StatusCode == http.StatusUnauthorized:
-			errors.As(runtime.NewResponseError(resp), &respErr)
-			reauth = err == nil &&
-				bloberror.HasCode(respErr, bloberror.InvalidAuthenticationInfo) &&
-				len(respErr.RawResponse.Header.Values("WWW-Authenticate")) != 0
-			if reauth {
-				debugCtx = context.WithValue(debugCtx, destReauthDebugCause, destReauthDebugCauseInvalidAuthenticationInfo)
+			switch { // Is it an error we can resolve by re-authing?
+			case errors.As(err, &authReq):
+				reauth = true
+				debugCtx = context.WithValue(debugCtx, destReauthDebugCause, destReauthDebugCauseAuthenticationRequired)
+			case resp.StatusCode == http.StatusUnauthorized:
+				errors.As(runtime.NewResponseError(resp), &respErr)
+				reauth = err == nil &&
+					bloberror.HasCode(respErr, bloberror.InvalidAuthenticationInfo) &&
+					len(respErr.RawResponse.Header.Values("WWW-Authenticate")) != 0
+				if reauth {
+					debugCtx = context.WithValue(debugCtx, destReauthDebugCause, destReauthDebugCauseInvalidAuthenticationInfo)
+				}
 			}
-		}
 
-		if reauth { // If it is, pull the lock if we can, reauth
-			m := reauthLock.L.(*sync.Mutex)
+			if reauth { // If it is, pull the lock if we can, reauth
+				m := reauthLock.L.(*sync.Mutex)
 
-			if m.TryLock() { // Fetch the lock and try until we get auth.
-				for {
-					if ctx.Value(destReauthDebugNoPrompt) == nil {
-						_ = common.GetLifecycleMgr().Prompt("Authentication is required to continue the job. Reauthorize and continue?", common.PromptDetails{
-							PromptType: common.EPromptType.Reauth(),
-							ResponseOptions: []common.ResponseOption{
-								common.EResponseOption.Yes(),
-							},
-						})
-					}
-
-					_, err = d.cred.Authenticate(debugCtx, &policy.TokenRequestOptions{
-						Scopes: []string{},
-					})
-
-					// I (Adele Reed) was initially worried about every case
-					// Thinking about it further, the worst case is that the job ends automatically, or when the user asks it to end.
-					// To avoid having to handle every error, we'll catch the cancel case as a way to exit the routine, but otherwise
-					// we will let it happen, and just retry.
-					if err == nil {
-						break
-					} else {
-						if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
-							select {
-							case <-ctx.Done(): // If it was us, exit like asked.
-								return nil, err // If it was us, that's legitimately important.
-							default: // If it was them, we don't care.
-							}
-						} else {
-							common.GetLifecycleMgr().Info(fmt.Sprintf("Authentication failed, awaiting input to continue: %s", err))
+				if m.TryLock() { // Fetch the lock and try until we get auth.
+					for {
+						if ctx.Value(destReauthDebugNoPrompt) == nil {
+							_ = common.GetLifecycleMgr().Prompt("Authentication is required to continue the job. Reauthorize and continue?", common.PromptDetails{
+								PromptType: common.EPromptType.Reauth(),
+								ResponseOptions: []common.ResponseOption{
+									common.EResponseOption.Yes(),
+								},
+							})
 						}
 
-						time.Sleep(time.Second * 5)
+						_, err = d.cred.Authenticate(debugCtx, &policy.TokenRequestOptions{
+							Scopes: []string{},
+						})
+
+						// I (Adele Reed) was initially worried about every case
+						// Thinking about it further, the worst case is that the job ends automatically, or when the user asks it to end.
+						// To avoid having to handle every error, we'll catch the cancel case as a way to exit the routine, but otherwise
+						// we will let it happen, and just retry.
+						if err == nil {
+							break
+						} else {
+							if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+								select {
+								case <-ctx.Done(): // If it was us, exit like asked.
+									return nil, err // If it was us, that's legitimately important.
+								default: // If it was them, we don't care.
+								}
+							} else {
+								common.GetLifecycleMgr().Info(fmt.Sprintf("Authentication failed, awaiting input to continue: %s", err))
+							}
+
+							time.Sleep(time.Second * 5)
+						}
 					}
+
+					m.Unlock()
+					reauthLock.Broadcast()
+				} else { // Otherwise, wait for a signal that we can try again.
+					reauthLock.Wait()
 				}
 
-				m.Unlock()
-				reauthLock.Broadcast()
-			} else { // Otherwise, wait for a signal that we can try again.
-				reauthLock.Wait()
-			}
-
-			// Try the request once more
-			goto retry
-		} // If it wasn't, we won't retry, and we'll simply return the error.
+				// Try the request once more
+				goto retry
+			} // If it wasn't, we won't retry, and we'll simply return the error.
+		}
 	}
 
 	return resp, err
