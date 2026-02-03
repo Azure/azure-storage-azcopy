4c0c72ef8fc18b6f8b52d3290be5a09374dd027a
Wendi Onwuakpa <wonwuakpa@microsoft.com>
2026-02-03 17:25:19 -0500

    update version for release

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.32.0~preview.4"
+const AzcopyVersion = "10.32.0"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const GCPImportUserAgent = "GCPImport " + UserAgent
