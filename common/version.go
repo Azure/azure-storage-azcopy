876dec31ae659168ccee6bb3224b4af4863559ab
dphulkar <dphulkar@microsoft.com>
2026-02-17 22:12:09 +0000

    Upgrade go version to 1.24.13

--- common/version.go
+++ common/version.go
@@ -1,6 +1,6 @@
 package common
 
-const AzcopyVersion = "10.32.0"
+const AzcopyVersion = "10.32.1"
 const UserAgent = "AzCopy/" + AzcopyVersion
 const S3ImportUserAgent = "S3Import " + UserAgent
 const GCPImportUserAgent = "GCPImport " + UserAgent
