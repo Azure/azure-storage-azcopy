6e5922b2bbce891fa888a9f2e71c63940f8916ee
varshaj-msft <varsha.jagdale@gmail.com>
2026-05-07 11:36:33 -0700

    Fix readonly attribute handling for Azure Files uploads (#3455)
    * BugFix: Readonly attribute is not being set for objects in azure file target
    
    If source object has readonly attribute, we set it to false in prologue so the object can be uploaded to target. Then in epilogue, we set the readonly property as needed. The bug is introduced due to pointer copy. When readonly is set to false in epilogue, it also modifies the original properties object so epilogue also sets it as false.
    
    * Fix for issue seen with read-only source files on Azure Files targets
    
    ---------
    
    Co-authored-by: Shashank Shekhar Nayak <shnayak@microsoft.com>
    Co-authored-by: bewurman-msft <wurman.benjamin@microsoft.com>

--- ste/sender-azureFile.go
+++ ste/sender-azureFile.go
@@ -235,6 +235,8 @@ func (u *azureFileSenderBase) Prologue(state common.PrologueState) (destinationM
 	// able to upload any data to the file!). We'll set it in epilogue, if necessary.
 	creationProperties := u.smbPropertiesToApply
 	if creationProperties.Attributes != nil {
+		attrsCopy := *u.smbPropertiesToApply.Attributes
+		creationProperties.Attributes = &attrsCopy
 		creationProperties.Attributes.ReadOnly = false
 	}
 
@@ -248,6 +250,9 @@ func (u *azureFileSenderBase) Prologue(state common.PrologueState) (destinationM
 		minimalLwt := time.Unix(0, 0)
 		creationProperties.LastWriteTime = &minimalLwt
 	}
+
+	// Set this before file creation
+	createOptions.SMBProperties = &creationProperties
 
 	err := common.DoWithOverrideReadOnlyOnAzureFiles(u.ctx,
 		func() (interface{}, error) {
@@ -274,9 +279,6 @@ func (u *azureFileSenderBase) Prologue(state common.PrologueState) (destinationM
 			u.jptm.FailActiveUpload("Creating parent directory", err)
 		}
 
-		if creationProperties.Attributes != nil {
-			createOptions.SMBProperties = &creationProperties
-		}
 		// retrying file creation
 		err = common.DoWithOverrideReadOnlyOnAzureFiles(u.ctx,
 			func() (interface{}, error) {
