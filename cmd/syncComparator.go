29ea3ffe300d652561895a9485d14566ee5ad00d
Rose Dinh <rosedinh@microsoft.com>
2026-07-29 20:26:48 +0000

    Remove line

--- cmd/syncComparator.go
+++ cmd/syncComparator.go
@@ -352,10 +352,6 @@ func (f *syncDestinationComparator) compareSourceAndDestinationObject(
 				azcopyScanningLogger.Log(common.LogInfo, out)
 			}
 
-			if stdout {
-				glcm.Info(out)
-			}
-
 			if sourceObject.lastModifiedTime.IsZero() {
 				// invalid LMT
 				// assume metadata change
