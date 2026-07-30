69aff3d4f85f475ed419b920c3901f1adcf02ef4
Rose Dinh <rosedinh@microsoft.com>
2026-07-30 15:40:57 +0000

    Remove destination LMT check

--- cmd/syncComparator.go
+++ cmd/syncComparator.go
@@ -312,12 +312,6 @@ func (f *syncDestinationComparator) compareSourceAndDestinationObject(
 ) (dataChanged, metadataChanged bool) {
 
 	// Check if data has changed by comparing size and modification time
-	out := fmt.Sprintf("rosedinh: in compareSourceAndDestinationObject")
-
-	if azcopyScanningLogger != nil {
-		azcopyScanningLogger.Log(common.LogInfo, out)
-	}
-
 	if sourceObject.entityType != common.EEntityType.Folder() &&
 		sourceObject.size != destinationObject.size {
 		// Compare file sizes first
@@ -327,32 +321,17 @@ func (f *syncDestinationComparator) compareSourceAndDestinationObject(
 
 	if sourceObject.lastWriteTime.IsZero() || destinationObject.lastWriteTime.IsZero() {
 		// assume it changed as we can't compare
-		out = fmt.Sprintf("rosedinh: LWT is zero")
-
-		if azcopyScanningLogger != nil {
-			azcopyScanningLogger.Log(common.LogInfo, out)
-		}
 		return true, true
 	}
 
 	// Compare last write times
 	if sourceObject.lastWriteTime.Compare(destinationObject.lastWriteTime) != 0 {
-		out = fmt.Sprintf("rosedinh: LWT is diff")
-
-		if azcopyScanningLogger != nil {
-			azcopyScanningLogger.Log(common.LogInfo, out)
-		}
 		return true, true
 	}
 
 	if !f.orchestratorOptions.metaDataOnlySync {
 		// if metadata only sync is not enabled, return early
 		// and assume metadata change status to be same as data
-		out = fmt.Sprintf("rosedinh: no metadata only sync")
-
-		if azcopyScanningLogger != nil {
-			azcopyScanningLogger.Log(common.LogInfo, out)
-		}
 		return false, false
 	}
 
@@ -365,48 +344,19 @@ func (f *syncDestinationComparator) compareSourceAndDestinationObject(
 	if f.orchestratorOptions.fromTo.From() == common.ELocation.File() {
 		if !f.orchestratorOptions.lastSuccessfulSyncJobStartTime.IsZero() {
 
-			out = fmt.Sprintf("sourceObjectLmt %s, destinationObjectLmt %s, lastSuccessfulSyncJobStartTime %s", 
-				sourceObject.lastModifiedTime, destinationObject.lastModifiedTime, f.orchestratorOptions.lastSuccessfulSyncJobStartTime)
-
-			if azcopyScanningLogger != nil {
-				azcopyScanningLogger.Log(common.LogInfo, out)
-			}
-
 			if sourceObject.lastModifiedTime.IsZero() {
 				// invalid LMT
 				// assume metadata change
-				out = fmt.Sprintf("rosedinh: source LMT is zero")
-
-				if azcopyScanningLogger != nil {
-					azcopyScanningLogger.Log(common.LogInfo, out)
-				}
 				return false, true
 			} else {
-				out = fmt.Sprintf("rosedinh: checking LMT's")
-
-				if azcopyScanningLogger != nil {
-					azcopyScanningLogger.Log(common.LogInfo, out)
-				}
 				// else check if source or target changed after last successful job start time
-				return false, (sourceObject.lastModifiedTime.After(f.orchestratorOptions.lastSuccessfulSyncJobStartTime) || 
-							   destinationObject.lastModifiedTime.After(f.orchestratorOptions.lastSuccessfulSyncJobStartTime))
+				return false, sourceObject.lastModifiedTime.After(f.orchestratorOptions.lastSuccessfulSyncJobStartTime)
 			}
 		} else {
-			out = fmt.Sprintf("rosedinh: Last successful sync job start time is zero")
-
-			if azcopyScanningLogger != nil {
-				azcopyScanningLogger.Log(common.LogInfo, out)
-			}
 			// If last successful job start time can't be used, we assume it's changed
 			// this will lead to more work but it is necessary to maintain fidelity
 			return false, true
 		}
-	}
-
-	out = fmt.Sprintf("rosedinh: past Files block")
-
-	if azcopyScanningLogger != nil {
-		azcopyScanningLogger.Log(common.LogInfo, out)
 	}
 
 	if isNFSCopy {
