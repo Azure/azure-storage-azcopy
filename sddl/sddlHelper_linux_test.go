3c958b69a61f651f329232c43a7ebc3d2cbb1508
Varsha Jagdale (from Dev Box) <vajagdal@microsoft.com>
2026-08-11 11:47:27 -0700

    add // Copyright © 2026

--- sddl/sddlHelper_linux_test.go
+++ sddl/sddlHelper_linux_test.go
@@ -1,7 +1,7 @@
 //go:build linux
 // +build linux
 
-// Copyright Microsoft <wastore@microsoft.com>
+// Copyright © 2026 Microsoft <wastore@microsoft.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
