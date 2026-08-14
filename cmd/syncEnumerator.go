505400794128ac4a07b2ad98e256ec862a4fbe5e
varshaj-msft <varsha.jagdale@gmail.com>
2026-08-13 17:28:23 -0700

    During sync, when failure is to access source, report source access error instead of error stating "trying to sync between different resource types" (#3556)
    * handle error when the local source path doesn't exist during sync initialization
    
    * check sourceErr before checking destination

--- cmd/syncEnumerator.go
+++ cmd/syncEnumerator.go
@@ -213,7 +213,13 @@ func (cca *cookedSyncCmdArgs) InitEnumerator(ctx context.Context, enumeratorOpti
 	}
 
 	// verify that the traversers are targeting the same type of resources
-	sourceIsDir, _ := sourceTraverser.IsDirectory(true)
+	sourceIsDir, sourceErr := sourceTraverser.IsDirectory(true)
+	// If the source doesn't exist, return the error directly instead of falling through
+	// to the resource type mismatch check which produces a confusing error message.
+	if sourceErr != nil && !cca.fromTo.From().IsRemote() {
+		return nil, fmt.Errorf("failed to scan source %s due to %w", cca.source.Value, sourceErr)
+	}
+
 	destIsDir, err := destinationTraverser.IsDirectory(true)
 
 	var resourceMismatchError = errors.New("trying to sync between different resource types (either file <-> directory or directory <-> file) which is not allowed." +
